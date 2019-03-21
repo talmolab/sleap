ec1aa1ec9a06dc0fd72fa0fd3a30213baa0d9440
davidt0x <davidt0x@gmail.com>
2019-03-21 13:15:00 -0400

    Addes JSON output serialization.

--- tests/io/test_dataset.py
+++ tests/io/test_dataset.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import numpy as np
 
 from sleap.instance import Instance, Point
 from sleap.io.video import Video
@@ -21,3 +22,13 @@ def test_labels_json(tmpdir, multi_skel_vid_labels):
 
     # Lets load the labels back in and make sure we haven't lost anything.
     loaded_labels = Labels.load_json(json_file_path)
+
+    # Check that we have the same thing
+    for expected_label, label in zip(multi_skel_vid_labels.labels, loaded_labels.labels):
+        assert expected_label.frame_idx == label.frame_idx
+
+        # Compare the first frames of the videos
+        np.allclose(expected_label.video.get_frame(0), label.video.get_frame(0))
+
+        # Compare the instances
+        assert expected_label.instances == label.instances
