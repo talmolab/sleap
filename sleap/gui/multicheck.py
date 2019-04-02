377f2e827e6d3523619fa06ee4eaf9f6dbbaae5c
Nat Tabris <nat.tabris@gmail.com>
2019-04-02 10:20:30 -0400

    Minor changes in multicheck.py to better conform to PEP8.

--- sleap/gui/multicheck.py
+++ sleap/gui/multicheck.py
@@ -1,9 +1,16 @@
+"""
+Module for Qt Widget to show multiple checkboxes for selecting from a sequence of numbers.
+
+Example:
+    >>> mc = MultiCheckWidget(count=5, selected=[0,1],title="My Items")
+    >>> me.selectionChanged.connect(window.plot)
+    >>> window.layout.addWidget(mc)
+"""
 from PySide2.QtCore import QRectF, Signal
 from PySide2.QtWidgets import QGridLayout, QGroupBox, QButtonGroup, QCheckBox
 
 class MultiCheckWidget(QGroupBox):
-    """
-    A Qt Widget to show multiple checkboxes for selecting from a sequence of numbers.
+    """Qt Widget to show multiple checkboxes for selecting from a sequence of numbers.
 
     Args:
         count (int): The number of checkboxes to show.
@@ -12,41 +19,41 @@ class MultiCheckWidget(QGroupBox):
         default (bool, optional): Default to checked/unchecked (ignored if selected arg given).
     """
 
-    def __init__(self, count, title="", selected=None, default=False, *args, **kwargs):
+    def __init__(self, *args, count, title="", selected=None, default=False, **kwargs):
         super(MultiCheckWidget, self).__init__(*args, **kwargs)
-        
+
         # QButtonGroup is the logical container
         # it allows us to get list of checked boxes more easily
         self.check_group = QButtonGroup()
         self.check_group.setExclusive(False) # more than one can be checked
-        
-        if len(title):
+
+        if title != "":
             self.setTitle(title)
             self.setFlat(False)
         else:
             self.setFlat(True)
-            
+
         if selected is None:
             selected = list(range(count)) if default else []
-        
+
         check_layout = QGridLayout()
         self.setLayout(check_layout)
         for i in range(count):
             check = QCheckBox("%d"%(i))
-            check.stateChanged.connect(lambda e: self.selectionChanged.emit()) # call signal/slot on self            
+            # call signal/slot on self when one of the checkboxes is changed
+            check.stateChanged.connect(lambda e: self.selectionChanged.emit())
             self.check_group.addButton(check, i)
             check_layout.addWidget(check, i//8, i%8)
         self.setSelected(selected)
-    
+
     """
     selectionChanged signal is sent whenever one of the checkboxes gets a stateChanged signal.
     """
     selectionChanged = Signal()
-    
+
     def getSelected(self) -> list:
-        """
-        Method to get list of the checked checkboxes.
-        
+        """Method to get list of the checked checkboxes.
+
         Returns:
             list of checked checkboxes
         """
@@ -55,14 +62,13 @@ class MultiCheckWidget(QGroupBox):
             if check_button.isChecked():
                 selected.append(self.check_group.id(check_button))
         return selected
-        
+
     def setSelected(self, selected: list):
-        """
-        Method to set some checkboxes as checked.
-        
+        """Method to set some checkboxes as checked.
+
         Args:
             selected (list): List of checkboxes to check.
-            
+
         Returns:
             None
         """
@@ -71,9 +77,13 @@ class MultiCheckWidget(QGroupBox):
                 check_button.setChecked(True)
             else:
                 check_button.setChecked(False)
-    
+
     def boundingRect(self) -> QRectF:
+        """Method required by Qt.
+        """
         return QRectF()
 
     def paint(self, painter, option, widget=None):
+        """Method required by Qt.
+        """
         pass
