c98cba6906954b50aabc2ba07d0f154ca8467519
Nat Tabris <nat.tabris@gmail.com>
2019-04-02 06:23:05 -0400

    Move data reads from gui test files into videos fixture.

--- tests/gui/test_conf_maps_view.py
+++ tests/gui/test_conf_maps_view.py
@@ -3,22 +3,13 @@ from sleap.gui.confmapsplot import ConfMapsPlot
 
 from sleap.io.video import Video
 
-TEST_H5_FILE = "tests/data/hdf5_format_v1/training.scale=0.50,sigma=10.h5"
-TEST_H5_DSET = "/confmaps"
-TEST_H5_INPUT_FORMAT = "channels_first"
-
-
-
-
 import PySide2.QtCore as QtCore
 
-def test_gui_conf_maps(qtbot):
-    
-    hdf5_conf = Video.from_hdf5(file=TEST_H5_FILE, dataset=TEST_H5_DSET, input_format=TEST_H5_INPUT_FORMAT)
+def test_gui_conf_maps(qtbot, hdf5_confmaps):
     
     vp = QtVideoPlayer()
     vp.show()
-    conf_maps = ConfMapsPlot(hdf5_conf.get_frame(1))
+    conf_maps = ConfMapsPlot(hdf5_confmaps.get_frame(1))
     vp.view.scene.addItem(conf_maps)
     
     # make sure we're showing all the channels
--- tests/gui/test_quiver.py
+++ tests/gui/test_quiver.py
@@ -3,21 +3,14 @@ from sleap.gui.quiverplot import MultiQuiverPlot
 
 from sleap.io.video import Video
 
-TEST_H5_FILE = "tests/data/hdf5_format_v1/training.scale=0.50,sigma=10.h5"
-TEST_H5_DSET = "/pafs"
-TEST_H5_INPUT_FORMAT = "channels_first"
-
-
 import PySide2.QtCore as QtCore
 
-def test_gui_quiver(qtbot):
-    
-    hdf5_conf = Video.from_hdf5(file=TEST_H5_FILE, dataset=TEST_H5_DSET, input_format=TEST_H5_INPUT_FORMAT)
+def test_gui_quiver(qtbot, hdf5_affinity):
     
     vp = QtVideoPlayer()
     vp.show()
     affinity_fields = MultiQuiverPlot(
-                    frame=hdf5_conf.get_frame(0),
+                    frame=hdf5_affinity.get_frame(0),
                     show=[0,1],
                     decimation=1
                     )
