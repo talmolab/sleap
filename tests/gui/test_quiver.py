9f85bae98a0f613f8ed754206e6a20bd19917aa1
Nat Tabris <nat.tabris@gmail.com>
2019-04-09 21:07:49 -0400

    Run QuiverPlot test on small slice of image so it's faster

--- tests/gui/test_quiver.py
+++ tests/gui/test_quiver.py
@@ -10,7 +10,7 @@ def test_gui_quiver(qtbot, hdf5_affinity):
     vp = QtVideoPlayer()
     vp.show()
     affinity_fields = MultiQuiverPlot(
-                    frame=hdf5_affinity.get_frame(0),
+                    frame=hdf5_affinity.get_frame(0)[265:275,238:248],
                     show=[0,1],
                     decimation=1
                     )
@@ -19,4 +19,4 @@ def test_gui_quiver(qtbot, hdf5_affinity):
     # make sure we're showing all the channels we selected
     assert len(affinity_fields.childItems()) == 2
     # make sure we're showing all arrows in first channel
-    assert len(affinity_fields.childItems()[0].childItems()) == 1148
+    assert len(affinity_fields.childItems()[0].childItems()) == 320
