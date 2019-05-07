26130ac24faa3ad854c06244892ce6d187cf84e0
Talmo <talmo@princeton.edu>
2019-05-07 16:06:29 -0400

    Added scaling to augmenter

--- sleap/nn/augmentation.py
+++ sleap/nn/augmentation.py
@@ -4,30 +4,49 @@ import keras
 import imgaug
 
 class Augmenter(keras.utils.Sequence):
-    def __init__(self, X, Y, output_names=None, batch_size=32, shuffle=True, rotation=(-180, 180)):
+    def __init__(self, X, Y, output_names=None, batch_size=32, shuffle=True, rotation=(-180, 180), scale=None):
         self.X = X
         self.Y = Y
         self.output_names = output_names
         self.num_outputs = 1 if output_names is None else len(output_names)
         self.batch_size = batch_size
         self.num_samples = len(X)
-        
+        self.rotation = rotation
+        self.scale = scale
+
+        # Setup batching
         all_idx = np.arange(self.num_samples)
         self.batches = np.array_split(all_idx, np.ceil(self.num_samples / self.batch_size))
         
+        # Initial shuffling
         if shuffle:
             self.shuffle()
+
+        # Setup affine augmentation
+        # TODO: translation?
+        self.aug_stack = []
+        if self.rotation is not None:
+            self.rotation = rotation if isinstance(rotation, tuple) else (-rotation, rotation)
+            if self.scale is not None and self.scale[0] != self.scale[1]:
+                self.scale = (min(self.scale), max(self.scale))
+                self.aug_stack.append(imgaug.augmenters.Affine(rotate=self.rotation, scale=self.scale))
+            else:
+                self.aug_stack.append(imgaug.augmenters.Affine(rotate=self.rotation))
         
-        self.aug = imgaug.augmenters.Sequential([
-            # imgaug.augmenters.Affine(rotate=(-15, 15), scale=(0.9, 1.1)),
-            imgaug.augmenters.Affine(rotate=rotation),
-            # imgaug.augmenters.Fliplr(0.5),
-        ])
+        # TODO: Flips?
+        # imgaug.augmenters.Fliplr(0.5)
+
+        # Create augmenter
+        self.aug = imgaug.augmenters.Sequential(self.aug_stack)
         
     def shuffle(self, batches_only=False):
+        """ Index-based shuffling """
+
         if batches_only:
+            # Shuffle batch order
             np.random.shuffle(self.batches)
         else:
+            # Re-batch after shuffling
             all_idx = np.arange(self.num_samples)
             np.random.shuffle(all_idx)
             self.batches = np.array_split(all_idx, np.ceil(self.num_samples / self.batch_size))
