4cb4f1c0ea204c453a478cd2bc8f6516a7097bf2
Nat Tabris <nat.tabris@gmail.com>
2019-05-19 14:13:58 -0400

    Support for file_open and file_dir types in FormBuilderLayout.

--- sleap/gui/formbuilder.py
+++ sleap/gui/formbuilder.py
@@ -1,6 +1,6 @@
 """Custom QFormLayout which populates itself from list of form fields."""
 
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore
 
 class FormBuilderLayout(QtWidgets.QFormLayout):
     """
@@ -10,6 +10,8 @@ class FormBuilderLayout(QtWidgets.QFormLayout):
         items_to_create: list which gets passed to get_form_data()
                          (see there for details about format)
     """
+
+    valueChanged = QtCore.Signal()
 
     def __init__(self, items_to_create, *args, **kwargs):
         super(FormBuilderLayout, self).__init__(*args, **kwargs)
@@ -82,9 +84,38 @@ class FormBuilderLayout(QtWidgets.QFormLayout):
                 for opt in item["options"].split(","):
                     field.addItem(opt)
                 # set default
+                if item["default"] in item["options"].split(","):
+                    idx = item["options"].split(",").index(item["default"])
+                    field.setCurrentIndex(idx)
             else:
                 field = QtWidgets.QLineEdit()
                 field.setText(item["default"])
+                if item["type"].split("_")[0] == "file":
+                    field.setDisabled(True)
     
             field.setObjectName(item["name"])
             self.addRow(item["label"] + ":", field)
+
+            if item["type"].split("_")[0] == "file":
+                file_button = QtWidgets.QPushButton("Select "+item["label"])
+
+                if item["type"].split("_")[-1] == "open":
+                    # Define function for button to trigger
+                    def select_file(*args, x=field):
+                        filter = item.get("filter", "Any File (*.*)")
+                        filename, _ = QtWidgets.QFileDialog.getOpenFileName(None, directory=None, caption="Open File", filter=filter)
+                        if len(filename): x.setText(filename)
+                        self.valueChanged.emit()
+
+                elif item["type"].split("_")[-1] == "dir":
+                    # Define function for button to trigger
+                    def select_file(*args, x=field):
+                        filename = QtWidgets.QFileDialog.getExistingDirectory(None, directory=None, caption="Open File")
+                        if len(filename): x.setText(filename)
+                        self.valueChanged.emit()
+
+                else:
+                    select_file = lambda: print(f"no action set for type {item['type']}")
+
+                file_button.clicked.connect(select_file)
+                self.addRow("", file_button)
