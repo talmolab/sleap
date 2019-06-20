c6fc688424d025e62a06724fa65d0ae5547de736
Nat Tabris <nat.tabris@gmail.com>
2019-06-20 17:17:34 -0400

    add instance_crops to import

--- sleap/nn/training.py
+++ sleap/nn/training.py
@@ -29,7 +29,7 @@ from sleap.io.dataset import Labels
 from sleap.nn.augmentation import Augmenter
 from sleap.nn.model import Model, ModelOutputType
 from sleap.nn.monitor import LossViewer
-from sleap.nn.datagen import generate_confmaps_from_points, generate_pafs_from_points, generate_images, generate_points
+from sleap.nn.datagen import generate_confmaps_from_points, generate_pafs_from_points, generate_images, generate_points, instance_crops
 
 
 @attr.s(auto_attribs=True)
