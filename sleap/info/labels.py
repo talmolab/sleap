dd2a75b00b2d410d651daed9a321f50d88d86d00
Nat Tabris <nat.tabris@gmail.com>
2019-07-12 16:00:41 -0400

    show info for first/last labeled frame

--- sleap/info/labels.py
+++ sleap/info/labels.py
@@ -10,3 +10,10 @@ if __name__ == "__main__":
     labels = Labels.load_json(args.data_path)
 
     print(f"Number of labeled frames: {len(labels)}")
+
+    first_idx = min((lf.frame_idx for lf in labels))
+    last_idx = max((lf.frame_idx for lf in labels))
+
+    print(f"  from {first_idx} to {last_idx}")
+
+    print(f"Video file: {labels.videos[0].filename}")
