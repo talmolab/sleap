3efd54faf4f2f908b0594417fdc8898371c40f09
Nat Tabris <nat.tabris@gmail.com>
2019-07-23 10:57:07 -0400

    show number of tracks/frame instances

--- sleap/info/labels.py
+++ sleap/info/labels.py
@@ -44,10 +44,18 @@ if __name__ == "__main__":
     print(f"Tracks: {len(labels.tracks)}")
 
     print(f"Video files:")
-    
-    for vid in labels.videos:
-        print(f"  {vid.filename}")
-        first_idx = min((lf.frame_idx for lf in labels.find(vid)))
-        last_idx = max((lf.frame_idx for lf in labels.find(vid)))
 
+    for vid in labels.videos:
+        lfs = labels.find(vid)
+
+        first_idx = min((lf.frame_idx for lf in lfs))
+        last_idx = max((lf.frame_idx for lf in lfs))
+
+        tracks = {inst.track for lf in lfs for inst in lf}
+        concurrent_count = max((len(lf.instances) for lf in lfs))
+
+        print(f"  {vid.filename}")
         print(f"    labeled from {first_idx} to {last_idx}")
+        print(f"    tracks: {len(tracks)}")
+        print(f"    max instances in frame: {concurrent_count}")
+
