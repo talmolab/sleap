e75bccdbaacde2ab79fc3a4b38e91a25e1aa7e98
Nat Tabris <nat.tabris@gmail.com>
2019-07-23 16:58:53 -0400

    compress track h5

--- sleap/info/write_tracking_h5.py
+++ sleap/info/write_tracking_h5.py
@@ -1,3 +1,4 @@
+import os
 import re
 import h5py as h5
 import numpy as np
@@ -11,7 +12,36 @@ if __name__ == "__main__":
     parser.add_argument("data_path", help="Path to labels json file")
     args = parser.parse_args()
 
-    labels = Labels.load_json(args.data_path)
+    def video_callback(video_list, new_paths=[os.path.dirname(args.data_path)]):
+        # Check each video
+        for video_item in video_list:
+            if "backend" in video_item and "filename" in video_item["backend"]:
+                current_filename = video_item["backend"]["filename"]
+                # check if we can find video
+                if not os.path.exists(current_filename):
+                    is_found = False
+
+                    current_basename = os.path.basename(current_filename)
+                    # handle unix, windows, or mixed paths
+                    if current_basename.find("/") > -1:
+                        current_basename = current_basename.split("/")[-1]
+                    if current_basename.find("\\") > -1:
+                        current_basename = current_basename.split("\\")[-1]
+
+                    # First see if we can find the file in another directory,
+                    # and if not, prompt the user to find the file.
+
+                    # We'll check in the current working directory, and if the user has
+                    # already found any missing videos, check in the directory of those.
+                    for path_dir in new_paths:
+                        check_path = os.path.join(path_dir, current_basename)
+                        if os.path.exists(check_path):
+                            # we found the file in a different directory
+                            video_item["backend"]["filename"] = check_path
+                            is_found = True
+                            break
+
+    labels = Labels.load_json(args.data_path, video_callback=video_callback)
 
     frame_count = len(labels)
     track_count = len(labels.tracks)
@@ -40,11 +70,15 @@ if __name__ == "__main__":
     print(f"tracks: {prediction_matrix.shape}")
 
     output_filename = re.sub("\.json(\.zip)?", "", args.data_path)
-    output_filename = output_filename + ".tracking.h5"
+    output_filename = output_filename + ".test_tracking.h5"
 
     with h5.File(output_filename, "w") as f:
         # We have to transpose the arrays since MATLAB expects column-major
-        ds = f.create_dataset("track_occupancy", data=np.transpose(occupancy_matrix))
-        ds = f.create_dataset("tracks", data=np.transpose(prediction_matrix))
+        ds = f.create_dataset(
+                "track_occupancy", data=np.transpose(occupancy_matrix),
+                compression="gzip", compression_opts=9)
+        ds = f.create_dataset(
+                "tracks", data=np.transpose(prediction_matrix),
+                compression="gzip", compression_opts=9)
 
     print(f"Saved as {output_filename}")
