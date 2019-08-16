f4cf735b76f52547ff845f599733db19bd08c307
Nat Tabris <nat.tabris@gmail.com>
2019-08-16 10:10:03 -0400

    revert training monitor Y range outlier code
    appears to be making the plot slower and doesn't always give
    a good range.

--- sleap/nn/monitor.py
+++ sleap/nn/monitor.py
@@ -137,15 +137,9 @@ class LossViewer(QtWidgets.QMainWindow):
         self.Y.append(y)
 
         dx = 0.5
+        dy = np.ptp(self.Y) * 0.02
         self.chart.axisX().setRange(min(self.X) - dx, max(self.X) + dx)
-
-        # Update Y range ever 100 points since it's slower to
-        # compute the quantiles
-        if len(self.Y) % 100 == 0:
-            dy = np.ptp(self.Y) * 0.04
-            low, high = np.quantile(self.Y, (.02, .98))
-
-            self.chart.axisY().setRange(low - dy, high + dy)
+        self.chart.axisY().setRange(max(1e-12, min(self.Y) - dy), max(self.Y) + dy)
 
     def set_start_time(self, t0):
         self.t0 = t0
