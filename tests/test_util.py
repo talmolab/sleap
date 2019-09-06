229802285877f5b2acc09b9dcd2eca51ee533b74
Nat Tabris <nat.tabris@gmail.com>
2019-09-06 16:52:40 -0400

    more tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,7 +4,7 @@ import pytest
 
 from typing import List, Dict
 
-from sleap.util import attr_to_dtype
+from sleap.util import attr_to_dtype, frame_list
 
 def test_attr_to_dtype():
     """
@@ -41,3 +41,6 @@ def test_attr_to_dtype():
     with pytest.raises(TypeError):
         attr_to_dtype(TestAttr3)
 
+def test_frame_list():
+    assert frame_list("3-5") == [3,4,5]
+    assert frame_list("7,10") == [7,10]
