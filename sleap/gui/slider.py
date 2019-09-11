58da178f08ff16a1f39055487b4987249f5eb600
Nat Tabris <nat.tabris@gmail.com>
2019-09-11 13:04:45 -0400

    support for header (graph), use mark objects
    also fix track colors so they match instance colors

--- sleap/gui/slider.py
+++ sleap/gui/slider.py
@@ -5,13 +5,55 @@ Drop-in replacement for QSlider with additional features.
 from PySide2.QtWidgets import QApplication, QWidget, QLayout, QAbstractSlider
 from PySide2.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsItem
 from PySide2.QtWidgets import QSizePolicy, QLabel, QGraphicsRectItem
-from PySide2.QtGui import QPainter, QPen, QBrush, QColor, QKeyEvent
-from PySide2.QtCore import Qt, Signal, QRect, QRectF
+from PySide2.QtGui import QPainter, QPen, QBrush, QColor, QKeyEvent, QPolygonF, QPainterPath
+from PySide2.QtCore import Qt, Signal, QRect, QRectF, QPointF
 
 from sleap.gui.overlays.tracks import TrackColorManager
 
-from operator import itemgetter
-from itertools import groupby
+import attr
+import itertools
+import numpy as np
+from typing import Union
+
+@attr.s(auto_attribs=True, cmp=False)
+class SliderMark:
+    type: str
+    val: float
+    end_val: float=None
+    row: int=None
+    track: 'Track'=None
+    _color: Union[tuple,str]="black"
+
+    @property
+    def color(self):
+        colors = dict(simple="black",
+                      filled="blue",
+                      open="blue",
+                      predicted="red")
+
+        if self.type in colors:
+            return colors[self.type]
+        else:
+            return self._color
+
+    @color.setter
+    def color(self, val):
+        self._color = val
+
+    @property
+    def QColor(self):
+        c = self.color
+        if type(c) == str:
+            return QColor(c)
+        else:
+            return QColor(*c)
+
+    @property
+    def filled(self):
+        if self.type == "open":
+            return False
+        else:
+            return True
 
 class VideoSlider(QGraphicsView):
     """Drop-in replacement for QSlider with additional features.
@@ -50,24 +92,28 @@ class VideoSlider(QGraphicsView):
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff) # ScrollBarAsNeeded
 
-        self._color_manager = color_manager or TrackColorManager()
+        self._color_manager = color_manager
 
+        self._track_rows = 0
         self._track_height = 3
+        self._header_height = 0
+        self._min_height = 19 + self._header_height
 
-        height = 19
-        slider_rect = QRect(0, 0, 200, height-3)
-        handle_width = 6
-        handle_rect = QRect(0, 1, handle_width, slider_rect.height()-2)
-        self.setMinimumHeight(height)
-        self.setMaximumHeight(height)
-
+        # Add border rect
+        slider_rect = QRect(0, 0, 200, self._min_height-3)
         self.slider = self.scene.addRect(slider_rect)
         self.slider.setPen(QPen(QColor("black")))
 
+        # Add drag handle rect
+        handle_width = 6
+        handle_rect = QRect(0, self._handleTop(), handle_width, self._handleHeight())
+        self.setMinimumHeight(self._min_height)
+        self.setMaximumHeight(self._min_height)
         self.handle = self.scene.addRect(handle_rect)
         self.handle.setPen(QPen(QColor(80, 80, 80)))
         self.handle.setBrush(QColor(128, 128, 128, 128))
 
+        # Add (hidden) rect to highlight selection
         self.select_box = self.scene.addRect(QRect(0, 1, 0, slider_rect.height()-2))
         self.select_box.setPen(QPen(QColor(80, 80, 255)))
         self.select_box.setBrush(QColor(80, 80, 255, 128))
@@ -82,6 +128,17 @@ class VideoSlider(QGraphicsView):
         self.setValue(val)
         self.setMarks(marks)
 
+        pen = QPen(QColor(80, 80, 255), .5)
+        pen.setCosmetic(True)
+        self.poly = self.scene.addPath(QPainterPath(), pen, self.select_box.brush())
+        self.headerSeries = dict()
+        self.drawHeader()
+
+    def _pointsToPath(self, points):
+        path = QPainterPath()
+        path.addPolygon(QPolygonF(points))
+        return path
+
     def setTracksFromLabels(self, labels, video):
         """Set slider marks using track information from `Labels` object.
 
@@ -91,66 +148,112 @@ class VideoSlider(QGraphicsView):
             labels: the `labels` with tracks and labeled_frames
             video: the video for which to show marks
         """
+
+        if self._color_manager is None:
+            self._color_manager = TrackColorManager(labels=labels)
+
         lfs = labels.find(video)
 
         slider_marks = []
-        track_idx = 0
+        track_row = 0
 
         # Add marks with track
         track_occupancy = labels.get_track_occupany(video)
         for track in labels.tracks:
-#             track_idx = labels.tracks.index(track)
             if track in track_occupancy and not track_occupancy[track].is_empty:
                 for occupancy_range in track_occupancy[track].list:
-                    slider_marks.append((track_idx, *occupancy_range))
-                track_idx += 1
+                    slider_marks.append(SliderMark("track", val=occupancy_range[0], end_val=occupancy_range[1], row=track_row, color=self._color_manager.get_color(track)))
+                track_row += 1
 
         # Add marks without track
         if None in track_occupancy:
             for occupancy_range in track_occupancy[None].list:
-                slider_marks.extend(range(*occupancy_range))
+                for val in range(*occupancy_range):
+                    slider_marks.append(SliderMark("simple", val=val))
 
         # list of frame_idx for simple markers for labeled frames
         labeled_marks = [lf.frame_idx for lf in lfs]
         user_labeled = [lf.frame_idx for lf in lfs if len(lf.user_instances)]
-        # "f" for suggestions with instances and "o" for those without
-        # "f" means "filled", "o" means "open"
-        # "p" for suggestions with only predicted instances
-        def mark_type(frame):
-            if frame in user_labeled:
-                return "f"
-            elif frame in labeled_marks:
-                return "p"
-            else:
-                return "o"
-        # list of (type, frame) tuples for suggestions
-        suggestion_marks = [(mark_type(frame_idx), frame_idx)
-            for frame_idx in labels.get_video_suggestions(video)]
-        # combine marks for labeled frame and marks for suggested frames
-        slider_marks.extend(suggestion_marks)
 
-        self.setTracks(track_idx)
+        for frame_idx in labels.get_video_suggestions(video):
+            if frame_idx in user_labeled:
+                mark_type = "filled"
+            elif frame_idx in labeled_marks:
+                mark_type = "predicted"
+            else:
+                mark_type = "open"
+            slider_marks.append(SliderMark(mark_type, val=frame_idx))
+
+        self.setTracks(track_row) # total number of tracks to show
         self.setMarks(slider_marks)
+
+        # self.setHeaderSeries(lfs)
 
         self.updatedTracks.emit()
 
-    def setTracks(self, tracks):
+    def setHeaderSeries(self, lfs):
+        # calculate total point distance for instances from last labeled frame
+        def inst_velocity(lf, last_lf):
+            val = 0
+            for inst in lf:
+                if last_lf is not None:
+                    last_inst = last_lf.find(track=inst.track)
+                    if last_inst:
+                        points_a = inst.points_array(invisible_as_nan=True)
+                        points_b = last_inst[0].points_array(invisible_as_nan=True)
+                        point_dist = np.linalg.norm(points_a - points_b, axis=1)
+                        inst_dist = np.sum(point_dist) # np.nanmean(point_dist)
+                        val += inst_dist if not np.isnan(inst_dist) else 0
+            return val
+
+        series = dict()
+
+        last_lf = None
+        for lf in lfs:
+            val = inst_velocity(lf, last_lf)
+            last_lf = lf
+            if not np.isnan(val):
+                series[lf.frame_idx] = val #len(lf.instances)
+
+        self.headerSeries = series
+        self.drawHeader()
+
+    def setTracks(self, track_rows):
         """Set the number of tracks to show in slider.
 
         Args:
-            tracks: the number of tracks to show
+            track_rows: the number of tracks to show
         """
+        self._track_rows = track_rows
+        self.updateHeight()
+
+    def updateHeight(self):
+        """Update the height of the slider."""
+
+        tracks = self._track_rows
         if tracks == 0:
-            min_height = max_height = 19
+            min_height = self._min_height
+            max_height = self._min_height
         else:
-            min_height = max(19, 8 + (self._track_height * min(tracks, 20)))
-            max_height = max(19, 8 + (self._track_height * tracks))
+            # Start with padding height
+            extra_height = 8 + self._header_height
+            min_height = extra_height
+            max_height = extra_height
+
+            # Add height for tracks
+            min_height += self._track_height * min(tracks, 20)
+            max_height += self._track_height * tracks
+
+            # Make sure min/max height is at least 19, even if few tracks
+            min_height = max(self._min_height, min_height)
+            max_height = max(self._min_height, max_height)
 
         self.setMaximumHeight(max_height)
         self.setMinimumHeight(min_height)
         self.resizeEvent()
 
     def _toPos(self, val, center=False):
+        """Convert value to x position on slider."""
         x = val
         x -= self._val_min
         x /= max(1, self._val_max-self._val_min)
@@ -160,6 +263,7 @@ class VideoSlider(QGraphicsView):
         return x
 
     def _toVal(self, x, center=False):
+        """Convert x position to value."""
         val = x
         val /= self._sliderWidth()
         val *= max(1, self._val_max-self._val_min)
@@ -304,6 +408,9 @@ class VideoSlider(QGraphicsView):
         self.clearMarks()
         if marks is not None:
             for mark in marks:
+                if not isinstance(mark, SliderMark):
+                    mark = SliderMark("simple", mark)
+                    print(mark)
                 self.addMark(mark, update=False)
         self.updatePos()
 
@@ -321,46 +428,29 @@ class VideoSlider(QGraphicsView):
             new_mark: value to mark
         """
         # check if mark is within slider range
-        if self._mark_val(new_mark) > self._val_max: return
-        if self._mark_val(new_mark) < self._val_min: return
+        if new_mark.val > self._val_max: return
+        if new_mark.val < self._val_min: return
 
         self._marks.add(new_mark)
 
+        v_top_pad = 3 + self._header_height
+        v_bottom_pad = 3
+
         width = 0
-        filled = True
-        if type(new_mark) == tuple:
-            if type(new_mark[0]) == int:
-                # colored track if mark has format: (track_number, start_frame_idx, end_frame_idx)
-                track = new_mark[0]
-                v_offset = 3 + (self._track_height * track)
-                height = 1
-                color = QColor(*self._color_manager.get_color(track))
-            else:
-                # rect (open/filled) if format: ("o", frame_idx) or ("f", frame_idx)
-                # ("p", frame_idx) when only predicted instances on frame
-                mark_type = new_mark[0]
-                v_offset = 3
-                height = self.slider.rect().height()-6
-                if mark_type == "o":
-                    width = 2
-                    filled = False
-                    color = QColor("blue")
-                elif mark_type == "f":
-                    width = 2
-                    color = QColor("blue")
-                elif mark_type == "p":
-                    width = 0
-                    color = QColor("red")
 
+        if new_mark.type == "track":
+            v_offset = v_top_pad + (self._track_height * new_mark.row)
+            height = 1
         else:
-            # line if mark has format: frame_idx
-            v_offset = 3
-            height = self.slider.rect().height()-6
-            color = QColor("black")
+            v_offset = v_top_pad
+            height = self.slider.rect().height()-(v_offset+v_bottom_pad)
 
+            width = 2 if new_mark.type in ("open", "filled") else 0
+
+        color = new_mark.QColor
         pen = QPen(color, .5)
         pen.setCosmetic(True)
-        brush = QBrush(color) if filled else QBrush()
+        brush = QBrush(color) if new_mark.filled else QBrush()
 
         line = self.scene.addRect(-width//2, v_offset, width, height,
                                   pen, brush)
@@ -368,32 +458,69 @@ class VideoSlider(QGraphicsView):
         if update: self.updatePos()
 
     def _mark_val(self, mark):
-        return mark[1] if type(mark) == tuple else mark
+        return mark.val
 
     def updatePos(self):
-        """Update the visual position of handle and slider annotations."""
+        """Update the visual x position of handle and slider annotations."""
         x = self._toPos(self.value())
         self.handle.setPos(x, 0)
+
         for mark in self._mark_items.keys():
+
             width = 0
-            if type(mark) == tuple:
-                in_track = True
-                v = mark[1]
-                if type(mark[0]) == int:
-                    width_in_frames = mark[2] - mark[1]
-                    width = max(2, self._toPos(width_in_frames))
-                elif mark[0] == "o":
-                    width = 2
-            else:
-                in_track = False
-                v = mark
-            x = self._toPos(v, center=True)
+            if mark.type == "track":
+                width_in_frames = mark.end_val - mark.val
+                width = max(2, self._toPos(width_in_frames))
+
+            elif mark.type in ("open", "filled"):
+                width = 2
+
+            x = self._toPos(mark.val, center=True)
             self._mark_items[mark].setPos(x, 0)
 
             rect = self._mark_items[mark].rect()
             rect.setWidth(width)
 
             self._mark_items[mark].setRect(rect)
+
+    def drawHeader(self):
+        if len(self.headerSeries) == 0 or self._header_height == 0:
+            self.poly.setPath(QPainterPath())
+            return
+
+        step = max(self.headerSeries.keys())//int(self._sliderWidth())
+        step = max(step, 1)
+        count = max(self.headerSeries.keys())//step*step
+
+        sampled = np.full((count), 0.0)
+        for key, val in self.headerSeries.items():
+            if key < count:
+                sampled[key] = val
+        sampled = np.max(sampled.reshape(count//step,step), axis=1)
+        series = {i*step:sampled[i] for i in range(count//step)}
+
+#         series = {key:self.headerSeries[key] for key in sorted(self.headerSeries.keys())}
+
+        series_min = np.min(sampled) - 1
+        series_max = np.max(sampled)
+        series_scale = (self._header_height-5)/(series_max - series_min)
+
+        def toYPos(val):
+            return self._header_height-((val-series_min)*series_scale)
+
+        step_chart = False # use steps rather than smooth line
+
+        points = []
+        points.append((self._toPos(0, center=True), toYPos(series_min)))
+        for idx, val in series.items():
+            points.append((self._toPos(idx, center=True), toYPos(val)))
+            if step_chart:
+                points.append((self._toPos(idx+step, center=True), toYPos(val)))
+        points.append((self._toPos(max(series.keys()) + 1, center=True), toYPos(series_min)))
+
+        # Convert to list of QPointF objects
+        points = list(itertools.starmap(QPointF,points))
+        self.poly.setPath(self._pointsToPath(points))
 
     def moveHandle(self, x, y):
         """Move handle in response to mouse position.
@@ -441,15 +568,27 @@ class VideoSlider(QGraphicsView):
 
         slider_rect.setHeight(height-3)
         if event is not None: slider_rect.setWidth(event.size().width()-1)
-        handle_rect.setHeight(slider_rect.height()-2)
-        select_box_rect.setHeight(slider_rect.height()-2)
+        handle_rect.setHeight(self._handleHeight())
+        select_box_rect.setHeight(self._handleHeight())
 
         self.slider.setRect(slider_rect)
         self.handle.setRect(handle_rect)
         self.select_box.setRect(select_box_rect)
 
         self.updatePos()
+        self.drawHeader()
         super(VideoSlider, self).resizeEvent(event)
+
+    def _handleTop(self):
+        return 1 + self._header_height
+
+    def _handleHeight(self, slider_rect=None):
+        if slider_rect is None:
+            slider_rect = self.slider.rect()
+
+        handle_bottom_offset = 1
+        handle_height = slider_rect.height() - (self._handleTop()+handle_bottom_offset)
+        return handle_height
 
     def mousePressEvent(self, event):
         """Override method to move handle for mouse press/drag.
@@ -521,6 +660,7 @@ class VideoSlider(QGraphicsView):
         """Method required by Qt."""
         super(VideoSlider, self).paint(*args, **kwargs)
 
+
 if __name__ == "__main__":
     app = QApplication([])
 
@@ -528,10 +668,7 @@ if __name__ == "__main__":
                 min=0, max=20, val=15,
                 marks=(10,15)#((0,10),(0,15),(1,10),(1,11),(2,12)), tracks=3
                 )
-    window.setTracks(5)
-#     mark_positions = ((0,10),(0,15),(1,10),(1,11),(2,12),(3,12),(3,13),(3,14),(4,15),(4,16),(4,21))
-    mark_positions = [("o",i) for i in range(3,15,4)] + [("f",18)]
-    window.setMarks(mark_positions)
+
     window.valueChanged.connect(lambda x: print(x))
     window.show()
 
