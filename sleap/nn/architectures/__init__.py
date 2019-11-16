5768ca373aa93ace466a95e2edb3c7b4e9daac10
Talmo Pereira <talmo@princeton.edu>
2019-11-16 11:08:53 -0500

    Add HigherHRNet and UDenseNet backbones.

--- sleap/nn/architectures/__init__.py
+++ sleap/nn/architectures/__init__.py
@@ -2,17 +2,37 @@ from sleap.nn.architectures.leap import LeapCNN
 from sleap.nn.architectures.unet import UNet, StackedUNet
 from sleap.nn.architectures.hourglass import StackedHourglass
 from sleap.nn.architectures.resnet import ResNet50
-from sleap.nn.architectures.densenet import DenseNet121, DenseNet169, DenseNet201, GeneralizedDenseNet
+from sleap.nn.architectures.densenet import (
+    DenseNet121,
+    DenseNet169,
+    DenseNet201,
+    GeneralizedDenseNet,
+    UDenseNet
+)
 from sleap.nn.architectures.mobilenets import MobileNetV1, MobileNetV2
 from sleap.nn.architectures.nasnet import NASNetMobile, NASNetLarge, GeneralizedNASNet
+from sleap.nn.architectures.hrnet import HigherHRNet
 from typing import TypeVar
+
 
 # TODO: We can set this up to find all classes under sleap.nn.architectures
 available_archs = [
-LeapCNN, UNet, StackedUNet, StackedHourglass, ResNet50,
-DenseNet121, DenseNet169, DenseNet201, GeneralizedDenseNet,
-MobileNetV1, MobileNetV2,
-NASNetMobile, NASNetLarge, GeneralizedNASNet
+    LeapCNN,
+    UNet,
+    StackedUNet,
+    StackedHourglass,
+    ResNet50,
+    DenseNet121,
+    DenseNet169,
+    DenseNet201,
+    GeneralizedDenseNet,
+    UDenseNet,
+    MobileNetV1,
+    MobileNetV2,
+    NASNetMobile,
+    NASNetLarge,
+    GeneralizedNASNet,
+    HigherHRNet,
 ]
 available_arch_names = [arch.__name__ for arch in available_archs]
 BackboneType = TypeVar("BackboneType", *available_archs)
--- sleap/nn/architectures/densenet.py
+++ sleap/nn/architectures/densenet.py
@@ -4,10 +4,18 @@ See original paper:
 https://arxiv.org/abs/1608.06993
 """
 
-import tensorflow as tf
-import keras_applications as applications
 import attr
+from typing import List, Tuple
 from sleap.nn.architectures import common
+import tensorflow as tf
+import numpy as np
+from keras_applications import densenet as bb
+
+layers = tf.keras.layers
+bb.backend = tf.keras.backend
+bb.layers = tf.keras.layers
+bb.models = tf.keras.models
+bb.keras_utils = tf.keras.utils
 
 
 @attr.s(auto_attribs=True)
@@ -57,7 +65,7 @@ class DenseNet121:
                 x = tf.keras.layers.Lambda(common.tile_channels)(x)
 
         # Automatically downloads weights
-        backbone_model = applications.densenet.DenseNet121(
+        backbone_model = bb.DenseNet121(
             include_top=False,
             input_shape=x.shape[1:],
             weights="imagenet" if self.pretrained else None,
@@ -65,17 +73,20 @@ class DenseNet121:
             backend=tf.keras.backend,
             layers=tf.keras.layers,
             models=tf.keras.models,
-            utils=tf.keras.utils
+            utils=tf.keras.utils,
         )
 
         # Output size is reduced by factor of 32 (2 ** 5)
         x = backbone_model(x)
 
         # Upsampling blocks.
-        x = common.upsampling_blocks(x,
+        x = common.upsampling_blocks(
+            x,
             up_blocks=self.up_blocks,
             upsampling_layers=self.upsampling_layers,
-            refine_conv_up=self.refine_conv_up, interp=self.interp)
+            refine_conv_up=self.refine_conv_up,
+            interp=self.interp,
+        )
 
         x = tf.keras.layers.Conv2D(num_output_channels, (3, 3), padding="same")(x)
 
@@ -142,7 +153,7 @@ class DenseNet169:
                 x = tf.keras.layers.Lambda(common.tile_channels)(x)
 
         # Automatically downloads weights
-        backbone_model = applications.densenet.DenseNet169(
+        backbone_model = bb.DenseNet169(
             include_top=False,
             input_shape=x.shape[1:],
             weights="imagenet" if self.pretrained else None,
@@ -150,17 +161,20 @@ class DenseNet169:
             backend=tf.keras.backend,
             layers=tf.keras.layers,
             models=tf.keras.models,
-            utils=tf.keras.utils
+            utils=tf.keras.utils,
         )
 
         # Output size is reduced by factor of 32 (2 ** 5)
         x = backbone_model(x)
 
         # Upsampling blocks.
-        x = common.upsampling_blocks(x,
+        x = common.upsampling_blocks(
+            x,
             up_blocks=self.up_blocks,
             upsampling_layers=self.upsampling_layers,
-            refine_conv_up=self.refine_conv_up, interp=self.interp)
+            refine_conv_up=self.refine_conv_up,
+            interp=self.interp,
+        )
 
         x = tf.keras.layers.Conv2D(num_output_channels, (3, 3), padding="same")(x)
 
@@ -227,7 +241,7 @@ class DenseNet201:
                 x = tf.keras.layers.Lambda(common.tile_channels)(x)
 
         # Automatically downloads weights
-        backbone_model = applications.densenet.DenseNet201(
+        backbone_model = bb.DenseNet201(
             include_top=False,
             input_shape=x.shape[1:],
             weights="imagenet" if self.pretrained else None,
@@ -235,17 +249,20 @@ class DenseNet201:
             backend=tf.keras.backend,
             layers=tf.keras.layers,
             models=tf.keras.models,
-            utils=tf.keras.utils
+            utils=tf.keras.utils,
         )
 
         # Output size is reduced by factor of 32 (2 ** 5)
         x = backbone_model(x)
 
         # Upsampling blocks.
-        x = common.upsampling_blocks(x,
+        x = common.upsampling_blocks(
+            x,
             up_blocks=self.up_blocks,
             upsampling_layers=self.upsampling_layers,
-            refine_conv_up=self.refine_conv_up, interp=self.interp)
+            refine_conv_up=self.refine_conv_up,
+            interp=self.interp,
+        )
 
         x = tf.keras.layers.Conv2D(num_output_channels, (3, 3), padding="same")(x)
 
@@ -284,7 +301,7 @@ class GeneralizedDenseNet:
             input resolution.
         refine_conv_up: If true, applies a 1x1 conv after each upsampling step.
     """
-    
+
     n_dense_blocks_1: int = 3
     n_dense_blocks_2: int = 6
     n_dense_blocks_3: int = 12
@@ -311,7 +328,7 @@ class GeneralizedDenseNet:
         x = x_in
 
         # Automatically downloads weights
-        backbone_model = applications.densenet.DenseNet(
+        backbone_model = bb.DenseNet(
             blocks=self.dense_blocks,
             include_top=False,
             input_shape=x.shape[1:],
@@ -320,17 +337,20 @@ class GeneralizedDenseNet:
             backend=tf.keras.backend,
             layers=tf.keras.layers,
             models=tf.keras.models,
-            utils=tf.keras.utils
+            utils=tf.keras.utils,
         )
 
         # Output size is reduced by factor of 32 (2 ** 5)
         x = backbone_model(x)
 
         # Upsampling blocks.
-        x = common.upsampling_blocks(x,
+        x = common.upsampling_blocks(
+            x,
             up_blocks=self.up_blocks,
             upsampling_layers=self.upsampling_layers,
-            refine_conv_up=self.refine_conv_up, interp=self.interp)
+            refine_conv_up=self.refine_conv_up,
+            interp=self.interp,
+        )
 
         x = tf.keras.layers.Conv2D(num_output_channels, (3, 3), padding="same")(x)
 
@@ -339,10 +359,10 @@ class GeneralizedDenseNet:
     @property
     def dense_blocks(self):
         return [
-        self.n_dense_blocks_1,
-        self.n_dense_blocks_2,
-        self.n_dense_blocks_3,
-        self.n_dense_blocks_4
+            self.n_dense_blocks_1,
+            self.n_dense_blocks_2,
+            self.n_dense_blocks_3,
+            self.n_dense_blocks_4,
         ]
 
     @property
@@ -357,3 +377,139 @@ class GeneralizedDenseNet:
         """Returns relative scaling factor of this backbone."""
 
         return 1 / (2 ** (self.down_blocks - self.up_blocks))
+
+
+def make_backbone(x_in, blocks, stem_stride=1, return_mid_feats=False):
+
+    x = tf.keras.layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(x_in)
+    x = tf.keras.layers.Conv2D(64, 7, strides=1, use_bias=False, name="conv1/conv")(x)
+    x = tf.keras.layers.BatchNormalization(epsilon=1.001e-5, name="conv1/bn")(x)
+    x = tf.keras.layers.Activation("relu", name="conv1/relu")(x)
+    x = tf.keras.layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
+    x = tf.keras.layers.MaxPooling2D(3, strides=stem_stride, name="pool1")(x)
+
+    x_mids = []
+    for i, block in enumerate(blocks):
+        x = bb.transition_block(x, 0.5, name=f"pool{i + 1}")
+        x = bb.dense_block(x, block, name=f"conv{i + 2}")
+        x_mids.append(x)
+
+    x = tf.keras.layers.BatchNormalization(epsilon=1.001e-5, name="bn")(x)
+    x = tf.keras.layers.Activation("relu", name="relu")(x)
+
+    model = tf.keras.Model(x_in, x, name="UDenseNet_backbone")
+
+    if return_mid_feats:
+        return model, x_mids
+    else:
+        return model
+
+
+def make_head(feats, output_channels, up_blocks, filters=64, mid_feats=None, name=""):
+    filters = np.array(filters)
+    if filters.size == 1:
+        filters = np.repeat(filters, up_blocks)
+
+    x = feats
+    x_mid_out = []
+    for i in range(up_blocks):
+        x = tf.keras.layers.UpSampling2D(interpolation="bilinear")(x)
+
+        if mid_feats is not None:
+            for j, mid_feat in enumerate(mid_feats):
+                if mid_feat.shape[1] == x.shape[1]:
+                    x = tf.keras.layers.Concatenate(axis=-1)([x, mid_feat])
+
+        x = tf.keras.layers.Conv2D(
+            filters=filters[i],
+            kernel_size=1,
+            strides=1,
+            padding="same",
+            activation="relu",
+        )(x)
+        x = tf.keras.layers.Conv2D(
+            filters=filters[i],
+            kernel_size=3,
+            strides=1,
+            padding="same",
+            activation="relu",
+        )(x)
+        x = tf.keras.layers.Conv2D(
+            filters=filters[i] // 2,
+            kernel_size=1,
+            strides=1,
+            padding="same",
+            activation="relu",
+        )(x)
+        x_mid_out.append(x)
+
+    x = tf.keras.layers.Conv2D(
+        filters=output_channels,
+        kernel_size=1,
+        strides=1,
+        padding="same",
+        activation=None,
+        name=f"{name}_out",
+    )(x)
+    return x, x_mid_out
+
+
+@attr.s(auto_attribs=True)
+class UDenseNet:
+    """UDenseNet backbone, a UNet-like architecture with skip connections to heads.
+
+    Attributes:
+        upsampling_layers: Use upsampling instead of transposed convolutions.
+        interp: Method to use for interpolation when upsampling smaller features.
+        up_blocks: Number of upsampling steps to perform. The backbone reduces
+            the output scale by 1/32. If set to 5, outputs will be upsampled to the
+            input resolution.
+        refine_conv_up: If true, applies a 1x1 conv after each upsampling step.
+    """
+
+    stem_stride: int = 1
+    dense_blocks: Tuple[int] = (2, 4, 6, 8)
+    output_scale: float = 1.0
+    n_heads: int = 1
+    head_filters: int = 64
+
+    def output(self, x_in, n_output_channels):
+        """Builds the layers for this backbone and return the output tensor.
+
+        Args:
+            x_in: Input 4-D tf.Tensor.
+            n_output_channels: Number of output channels.
+
+        Returns:
+            A tf.keras.Model with as many outputs as the n_heads attribute for this
+                model.
+        """
+
+        backbone, backbone_mid_feats = make_backbone(
+            x_in, self.dense_blocks, stem_stride=self.stem_stride, return_mid_feats=True
+        )
+
+        output_size = int(x_in.shape[1] * self.output_scale)
+        up_blocks = int(np.log(output_size / backbone.output_shape[1]) / np.log(2))
+
+        x = backbone.output
+        mid_feats = backbone_mid_feats
+        outputs = []
+        for head in self.n_heads:
+            x, mid_feats = make_head(
+                x,
+                n_output_channels,
+                up_blocks,
+                filters=self.head_filters,
+                mid_feats=mid_feats,
+                name=f"head{head + 1}",
+            )
+            outputs.append(x)
+
+        return tf.keras.Model(x_in, outputs, name="UDenseNet")
+
+    @property
+    def down_blocks(self):
+        """Returns the number of downsampling steps in the model."""
+
+        return int(np.log(self.stem_stride) / np.log(2)) + len(self.dense_blocks)
