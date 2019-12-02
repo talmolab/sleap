e3322f5c5993a48def96ca81b10aa83c31a6a818
Nat Tabris <nat.tabris@gmail.com>
2019-12-02 14:12:41 -0500

    Adjust param name for test.

--- tests/gui/test_suggestions.py
+++ tests/gui/test_suggestions.py
@@ -4,7 +4,7 @@ from sleap.gui.suggestions import VideoFrameSuggestions
 def test_velocity_suggestions(centered_pair_predictions):
     suggestions = VideoFrameSuggestions.suggest(
         labels=centered_pair_predictions,
-        params=dict(method="velocity", node_idx=0, threshold=0.5),
+        params=dict(method="velocity", node="", threshold=0.5),
     )
     assert len(suggestions) == 12
     assert suggestions[0].frame_idx == 80
