3e2c68a54d10d7459f742eeb7ccd911ac5f99050
Talmo <talmo@princeton.edu>
2020-02-06 04:18:00 -0500

    Add rank expansion data utility.

--- sleap/nn/data/utils.py
+++ sleap/nn/data/utils.py
@@ -7,3 +7,26 @@ def ensure_list(x: Any) -> List[Any]:
     if not isinstance(x, list):
         return [x]
     return x
+
+
+def expand_to_rank(x: tf.Tensor, target_rank: int, prepend: bool = True) -> tf.Tensor:
+    """Expand a tensor to a target rank by adding singleton dimensions.
+
+    Args:
+        x: Any `tf.Tensor` with rank <= `target_rank`.
+        target_rank: Rank to expand the input to.
+        prepend: If True, singleton dimensions are added before the first axis of the
+            data. If False, singleton dimensions are added after the last axis.
+
+    Returns:
+        The expanded tensor of the same dtype as the input, but with rank `target_rank`.
+
+        The output has the same exact data as the input tensor and will be identical if
+        they are both flattened.
+    """
+    singleton_dims = tf.ones([target_rank - tf.rank(x)], tf.int32)
+    if prepend:
+        new_shape = tf.concat([singleton_dims, tf.shape(x)], axis=0)
+    else:
+        new_shape = tf.concat([tf.shape(x), singleton_dims], axis=0)
+    return tf.reshape(x, shape=new_shape)
