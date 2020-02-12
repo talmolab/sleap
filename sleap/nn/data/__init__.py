eaff01438cf45b12f1946b85c4e8c0a357530fb3
Nat Tabris <nat.tabris@gmail.com>
2020-02-12 12:06:37 -0500

    Add pipelines submodule to nn.data.

--- sleap/nn/data/__init__.py
+++ sleap/nn/data/__init__.py
@@ -3,6 +3,7 @@ from sleap.nn.data import confidence_maps
 from sleap.nn.data import instance_centroids
 from sleap.nn.data import instance_cropping
 from sleap.nn.data import normalization
+from sleap.nn.data import pipelines
 from sleap.nn.data import providers
 from sleap.nn.data import resizing
 from sleap.nn.data import utils
