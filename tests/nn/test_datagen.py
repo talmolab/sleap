d3784ccc6c5d3e7de3ff6c10b490a29fa449af61
Nat Tabris <nat.tabris@gmail.com>
2020-02-12 11:28:02 -0500

    Use new pipeline in datagen test.

--- tests/nn/test_datagen.py
+++ tests/nn/test_datagen.py
@@ -6,33 +6,21 @@ def test_datagen(min_labels):
     import numpy as np
     import math
 
-    training_data = data.TrainingData.from_labels(min_labels)
-    ds = training_data.to_ds()
-
-    skeleton = min_labels.skeletons[0]
-
-    conf_data = data.make_confmap_dataset(ds,)
+    ds = data.pipelines.Pipeline.from_sequence(
+        [
+            data.providers.LabelsReader(min_labels),
+            data.confidence_maps.MultiConfidenceMapGenerator(),
+        ]
+    ).make_dataset()
 
     imgs = []
     confmaps = []
-    for img, confmap in conf_data:
-        if type(confmap) == tuple:
-            confmap = confmap[0]
-        imgs.append(img)
-        confmaps.append(confmap)
+    for item in ds:
+        imgs.append(item["image"])
+        confmaps.append(item["confidence_maps"])
 
     imgs = np.stack(imgs)
     confmaps = np.stack(confmaps)
-
-    paf_data = data.make_paf_dataset(
-        ds, data.SimpleSkeleton.from_skeleton(skeleton).edges,
-    )
-
-    pafs = []
-    for img, paf in paf_data.take(10):
-        pafs.append(paf)
-
-    pafs = np.stack(pafs)
 
     assert imgs.shape == (1, 384, 384, 1)
     assert math.isclose(np.ptp(imgs / 255), 0.898, abs_tol=0.01)
@@ -41,6 +29,20 @@ def test_datagen(min_labels):
     assert confmaps.dtype == np.dtype("float32")
     assert math.isclose(np.ptp(confmaps), 0.999, abs_tol=0.01)
 
-    assert pafs.shape == (1, 384, 384, 2)
+    skeleton = min_labels.skeletons[0]
+
+    ds = data.pipelines.Pipeline.from_sequence(
+        [
+            data.providers.LabelsReader(min_labels),
+            data.edge_maps.PartAffinityFieldsGenerator(skeletons=skeleton),
+        ]
+    ).make_dataset()
+
+    pafs = []
+    for item in ds:
+        pafs.append(item["part_affinity_fields"])
+
+    pafs = np.stack(pafs)
+
+    assert pafs.shape == (1, 384, 384, 1, 2)
     assert pafs.dtype == np.dtype("float32")
-    assert math.isclose(np.ptp(pafs), 1.57, abs_tol=0.01)
