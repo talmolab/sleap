ba205d8aa647a07e16804774f02db6bbac0f9708
Talmo <talmo@princeton.edu>
2020-02-13 17:05:39 -0500

    Add old config module docstring back in.

--- sleap/nn/config/training_job.py
+++ sleap/nn/config/training_job.py
@@ -1,3 +1,29 @@
+"""Serializable configuration classes for specifying all training job parameters.
+
+These configuration classes are intended to specify all the parameters required to run
+a training job or perform inference from a serialized one.
+
+They are explicitly not intended to implement any of the underlying functionality that
+they parametrize. This serves two purposes:
+
+    1. Parameter specification through simple attributes. These can be read/edited by a
+       human, as well as easily be serialized/deserialized to/from simple dictionaries
+       and JSON.
+
+    2. Decoupling from the implementation. This makes it easier to design functional
+       modules with attributes/parameters that contain objects that may not be easily
+       serializable or may implement additional logic that relies on runtime information
+       or other parameters.
+
+In general, classes that implement the actual functionality related to these
+configuration classes should provide a classmethod for instantiation from the
+configuration class instances. This makes it easier to implement other logic not related
+to the high level parameters at creation time.
+
+Conveniently, this format also provides a single location where all user-facing
+parameters are aggregated and documented for end users (as opposed to developers).
+"""
+
 import attr
 from sleap.nn.config.data import DataConfig
 from sleap.nn.config.model import ModelConfig
