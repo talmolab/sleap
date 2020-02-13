74bc2c99410246cce3e0be171d8952a3fb0af5da
Talmo <talmo@princeton.edu>
2020-02-13 01:49:31 -0500

    Add LabelsReader splitting utility function

--- sleap/nn/data/training.py
+++ sleap/nn/data/training.py
@@ -2,11 +2,81 @@
 
 import numpy as np
 import tensorflow as tf
+from sleap.nn.data.providers import LabelsReader
 from sleap.nn.data.utils import expand_to_rank, ensure_list
 import attr
-from typing import List, Text, Optional, Any, Union, Dict
+from typing import List, Text, Optional, Any, Union, Dict, Tuple, Sequence
 
 
+def split_labels_reader(
+    labels_reader: LabelsReader, split_fractions: Sequence[float]
+) -> Tuple[LabelsReader]:
+    """Split a `LabelsReader` into multiple new ones with random subsets of the data.
+
+    Args:
+        labels_reader: An instance of `sleap.nn.data.providers.LabelsReader`. This is a
+            provider that generates datasets that contain elements read from a
+            `sleap.Labels` instance.
+        split_fractions: One or more floats between 0 and 1 that specify the fraction of
+            examples that should be in each dataset. These should add up to <= 1.0.
+            Fractions of less than 1 element will be rounded up to ensure that is at
+            least 1 element in each split. One of the fractions may be -1 to indicate
+            that it should contain all elements left over from the other splits.
+
+    Returns:
+        A tuple of `LabelsReader` instances of the same length as `split_fractions`. The
+        indices will be stored in the `example_indices` in each `LabelsReader` instance.
+
+        The actual `sleap.Labels` instance will be the same for each instance, only the
+        `example_indices` that are iterated over will change across splits.
+
+        If the input `labels_reader` already has `example_indices`, a subset of these
+        will be sampled to generate the splits.
+
+    Raises:
+        ValueError: If more than one split fraction is specified as -1.
+        ValueError: If the splits add up to more than the total available examples.
+
+    Note:
+        Sampling is done without replacement.
+    """
+    # Get available indices.
+    labels_indices = labels_reader.example_indices
+    if labels_indices is None:
+        labels_indices = np.arange(len(labels_reader))
+    labels_indices = np.array(labels_indices).astype("int64")
+
+    # Compute split sizes.
+    n_examples = len(labels_indices)
+    n_examples_per_split = np.array(split_fractions).astype("float64")
+    if (n_examples_per_split == -1).sum() > 1:
+        raise ValueError("Only one split fraction can be specified as -1.")
+    n_examples_per_split[n_examples_per_split == -1] = np.NaN
+    n_examples_per_split = np.ceil(n_examples_per_split * n_examples)
+    n_examples_per_split[np.isnan(n_examples_per_split)] = np.maximum(
+        n_examples - np.nansum(n_examples_per_split), 1
+    )
+    n_examples_per_split = n_examples_per_split.astype("int64")
+    if n_examples_per_split.sum() > n_examples:
+        raise ValueError("Splits cannot sum to more than the total input labels.")
+
+    # Sample and create new LabelsReader instances.
+    split_readers = []
+    for n_samples in n_examples_per_split:
+        # Sample.
+        sampled_indices = np.random.default_rng().choice(
+            labels_indices, size=n_samples, replace=False
+        )
+
+        # Create new instance.
+        split_readers.append(
+            LabelsReader(labels_reader.labels, example_indices=sampled_indices)
+        )
+
+        # Exclude the sampled indices from the available indices.
+        labels_indices = np.setdiff1d(labels_indices, sampled_indices)
+
+    return tuple(split_readers)
 
 
 @attr.s(auto_attribs=True)
@@ -22,7 +92,9 @@ class KeyMapper:
             in the same order.
     """
 
-    key_maps: List[Dict[Text, Text]] = attr.ib(converter=attr.converters.optional(ensure_list))
+    key_maps: List[Dict[Text, Text]] = attr.ib(
+        converter=attr.converters.optional(ensure_list)
+    )
 
     @property
     def input_keys(self) -> List[Text]:
@@ -51,12 +123,15 @@ class KeyMapper:
             A dataset that generates examples with the tensors in `input_keys` mapped to
             keys in `output_keys` according to the structure in `key_maps`.
         """
+
         def map_keys(example):
             """Local processing function for dataset mapping."""
             output_keys = []
             for key_map in self.key_maps:
-                output_keys.append({key_out: example[key_in] for key_in, key_out in key_map.items()})
+                output_keys.append(
+                    {key_out: example[key_in] for key_in, key_out in key_map.items()}
+                )
             return tuple(output_keys)
+
         ds_output = ds_input.map(map_keys)
         return ds_output
-
--- tests/nn/data/test_training.py
+++ tests/nn/data/test_training.py
@@ -3,7 +3,37 @@ import tensorflow as tf
 
 tf.config.experimental.set_visible_devices([], device_type="GPU")  # hide GPUs for test
 
+import sleap
+from sleap.nn.data.providers import LabelsReader
 from sleap.nn.data import training
+
+
+def test_split_labels_reader(min_labels):
+    labels = sleap.Labels([min_labels[0], min_labels[0], min_labels[0], min_labels[0]])
+    labels_reader = LabelsReader(labels)
+    reader1, reader2 = training.split_labels_reader(labels_reader, [0.5, 0.5])
+    assert len(reader1) == 2
+    assert len(reader2) == 2
+    assert len(set(reader1.example_indices).intersection(set(reader2.example_indices))) == 0
+
+    reader1, reader2 = training.split_labels_reader(labels_reader, [0.1, 0.5])
+    assert len(reader1) == 1
+    assert len(reader2) == 2
+    assert len(set(reader1.example_indices).intersection(set(reader2.example_indices))) == 0
+
+    reader1, reader2 = training.split_labels_reader(labels_reader, [0.1, -1])
+    assert len(reader1) == 1
+    assert len(reader2) == 3
+    assert len(set(reader1.example_indices).intersection(set(reader2.example_indices))) == 0
+
+    labels = sleap.Labels([min_labels[0], min_labels[0], min_labels[0], min_labels[0]])
+    labels_reader = LabelsReader(labels, example_indices=[1, 2, 3])
+    reader1, reader2 = training.split_labels_reader(labels_reader, [0.1, -1])
+    assert len(reader1) == 1
+    assert len(reader2) == 2
+    assert len(set(reader1.example_indices).intersection(set(reader2.example_indices))) == 0
+    assert 0 not in reader1.example_indices
+    assert 0 not in reader2.example_indices
 
 
 def test_keymapper():
