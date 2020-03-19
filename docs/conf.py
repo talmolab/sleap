d1b93be76a679602d311480a59d2c9b4da6ee264
Nat Tabris <nat.tabris@gmail.com>
2020-03-19 13:27:12 -0400

    Use small font size for website.

--- docs/conf.py
+++ docs/conf.py
@@ -79,13 +79,15 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'font_size': '15px'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
