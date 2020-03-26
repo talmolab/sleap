4fd6dd965341d6c697feb17ca234fa24854e0686
Nat Tabris <nat.tabris@gmail.com>
2020-03-26 14:32:54 -0400

    Add sleap-convert cli for file format conversions.
    You can input a SLEAP dataset or something than can be translated
    into a SLEAP dataset (i.e., DeepLabCut, COCO) and then output
    either a SLEAP dataset (in slp/h5 or json) or a "tracking" h5
    with a format that's better for analysis (e.g., using in MATLAB
    or something else downstream from SLEAP).

---
+++ sleap/io/convert.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+import re
+
+from sleap import Labels
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path", help="Path to input file.")
+    parser.add_argument(
+        "-o", "--output", default="", help="Path to output file (optional)."
+    )
+    parser.add_argument(
+        "--format",
+        default="slp",
+        help="Output format. Default ('slp') is SLEAP dataset; "
+        "'tracking' results in tracking.h5 file; "
+        "'h5' or 'json' results in SLEAP dataset "
+        "with specified file format.",
+    )
+
+    args = parser.parse_args()
+
+    video_callback = Labels.make_video_callback([os.path.dirname(args.input_path)])
+    try:
+        labels = Labels.load_file(args.input_path, video_callback=video_callback)
+    except TypeError:
+        print("Input file isn't SLEAP dataset so attempting other importers...")
+        from sleap.io.format import read
+
+        labels = read(
+            args.input_path,
+            for_object="labels",
+            as_format="*",
+            video_callback=video_callback,
+        )
+
+    if args.format == "tracking":
+        from sleap.info.write_tracking_h5 import main as write_tracking
+
+        if args.output:
+            output_path = args.output
+        else:
+            output_path = args.input_path
+            output_path = re.sub("(\.json(\.zip)?|\.h5|\.slp)$", "", output_path)
+            output_path = output_path + ".tracking.h5"
+
+        write_tracking(labels, output_path=output_path, all_frames=True)
+
+    elif args.output:
+        print(f"Output SLEAP dataset: {args.output}")
+        Labels.save_file(labels, args.output)
+
+    elif args.format in ("slp", "h5", "json"):
+        output_path = f"{args.input_path}.{args.format}"
+        print(f"Output SLEAP dataset: {output_path}")
+        Labels.save_file(labels, output_path)
+
+    else:
+        print("You didn't specify how to convert the file.")
+        print(args)
+
+
+if __name__ == "__main__":
+    main()
