1bb5c6da591a7e39084767c6ce50446fd94493e1
Nat Tabris <nat.tabris@gmail.com>
2020-05-29 16:44:02 -0400

    Add --verbose for inst info in sleap-inspect.

--- sleap/info/labels.py
+++ sleap/info/labels.py
@@ -11,6 +11,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("data_path", help="Path to labels json file")
+    parser.add_argument("--verbose", default=False, action="store_true")
     args = parser.parse_args()
 
     video_callback = Labels.make_video_callback([os.path.dirname(args.data_path)])
@@ -24,7 +25,14 @@ def main():
     total_user_frames = 0
 
     for vid in labels.videos:
+        print(f"  {vid.filename}")
+
         lfs = labels.find(vid)
+
+        print(f"    labeled frames: {len(lfs)}")
+
+        if not lfs:
+            continue
 
         first_idx = min((lf.frame_idx for lf in lfs))
         last_idx = max((lf.frame_idx for lf in lfs))
@@ -35,12 +43,24 @@ def main():
 
         total_user_frames += user_frames
 
-        print(f"  {vid.filename}")
         print(f"    labeled frames from {first_idx} to {last_idx}")
-        print(f"    labeled frames: {len(lfs)}")
         print(f"    user labeled frames: {user_frames}")
         print(f"    tracks: {len(tracks)}")
         print(f"    max instances in frame: {concurrent_count}")
+
+        if args.verbose:
+            print()
+            print("    labeled frames:              bounding box top left (x, y)")
+            for lf in lfs:
+                bb_cords = [
+                    f"({inst.bounding_box[0]:.2f}, {inst.bounding_box[1]:.2f}){'^' if hasattr(inst, 'score') else ''}"
+                    for inst in lf.instances
+                ]
+                pt_str = " ".join(bb_cords)
+                print(
+                    f"      frame {lf.frame_idx}: {len(lf.instances)} instances -> {pt_str}"
+                )
+            print()
 
     print(f"Total user labeled frames: {total_user_frames}")
 
