a4683904c3bdc25b000042e3cca37833d4629319
Nat Tabris <nat.tabris@gmail.com>
2020-06-29 12:14:08 -0400

    Fix test for when env var not set.

--- tests/gui/test_filedialog.py
+++ tests/gui/test_filedialog.py
@@ -6,7 +6,7 @@ from sleap.gui.dialogs.filedialog import FileDialog
 
 
 def test_non_native_dialog():
-    save_env_non_native = os.environ["USE_NON_NATIVE_FILE"]
+    save_env_non_native = os.environ.get("USE_NON_NATIVE_FILE", None)
 
     os.environ["USE_NON_NATIVE_FILE"] = ""
 
@@ -19,4 +19,5 @@ def test_non_native_dialog():
     FileDialog._non_native_if_set(d)
     assert d["options"] == QtWidgets.QFileDialog.DontUseNativeDialog
 
-    os.environ["USE_NON_NATIVE_FILE"] = save_env_non_native
+    if save_env_non_native is not None:
+        os.environ["USE_NON_NATIVE_FILE"] = save_env_non_native
