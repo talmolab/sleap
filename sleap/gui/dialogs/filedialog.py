066e7e7be45450d386947c4c1430da735515604a
Nat Tabris <nat.tabris@gmail.com>
2020-06-29 10:44:05 -0400

    Add file ext in non-native file dialog.
    Native Mac/Win file dialogs add file ext based on selected file
    type but non-native dialog (used for Linux) didn't. Now it does.
    
    Resolves issue #370.

--- sleap/gui/dialogs/filedialog.py
+++ sleap/gui/dialogs/filedialog.py
@@ -2,7 +2,7 @@
 Wrappers for Qt File Dialogs.
 """
 
-import os, sys
+import os, re, sys
 
 from PySide2 import QtWidgets
 
@@ -40,8 +40,31 @@ class FileDialog(object):
 
         Passes along everything except empty "options" arg.
         """
-        cls._non_native_if_set(kwargs)
-        return QtWidgets.QFileDialog.getSaveFileName(*args, **kwargs)
+        is_non_native = cls._non_native_if_set(kwargs)
+
+        # The non-native file dialog doesn't add file extensions from the
+        # file-type menu in the dialog, so we need to do this ourselves.
+        if is_non_native and "filter" in kwargs and "dir" in kwargs:
+            filename = kwargs["dir"]
+            filters = kwargs["filter"].split(";;")
+            if filters:
+                if ".slp" in filters[0] and not filename.endswith(".slp"):
+                    kwargs["dir"] = f"{filename}.slp"
+
+        filename, filter = QtWidgets.QFileDialog.getSaveFileName(*args, **kwargs)
+
+        # Make sure filename has appropriate file extension.
+        if is_non_native and filter:
+            # Get ext from selected filter (i.e., file type).
+            match = re.search("\\(\\*(\\.[a-z]+)", filter)
+            if match:
+                filter_ext = match[1]
+
+                # If ext isn't already at end of filename, add it.
+                if filter_ext and not filename.endswith(filter_ext):
+                    filename = f"{filename}{filter_ext}"
+
+        return filename, filter
 
     @classmethod
     def openDir(cls, *args, **kwargs):
@@ -54,14 +77,18 @@ class FileDialog(object):
         return QtWidgets.QFileDialog.getExistingDirectory(*args, **kwargs)
 
     @staticmethod
-    def _non_native_if_set(kwargs):
+    def _non_native_if_set(kwargs) -> bool:
+        is_non_native = False
         is_linux = sys.platform.startswith("linux")
         env_var_set = os.environ.get("USE_NON_NATIVE_FILE", False)
 
         if is_linux or env_var_set:
+            is_non_native = True
             kwargs["options"] = kwargs.get("options", 0)
             kwargs["options"] |= QtWidgets.QFileDialog.DontUseNativeDialog
 
         # Make sure we don't send empty options argument
         if "options" in kwargs and not kwargs["options"]:
             del kwargs["options"]
+
+        return is_non_native
