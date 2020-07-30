2bddfd65d3a9229333f841784f530def685a5406
Nat Tabris <nat.tabris@gmail.com>
2020-07-30 16:22:29 -0600

    Add docstring and test.

---
+++ tests/test_version.py
@@ -0,0 +1,11 @@
+import re
+
+from sleap.version import __version__
+
+
+def test_version():
+    with open("sleap/version.py") as f:
+        version_file = f.read()
+        sleap_version = re.search("\d.+(?=['\"])", version_file).group(0)
+
+        assert sleap_version == __version__
