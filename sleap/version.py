8ec782121757ec25933bd0bd5477b6f1ec17a102
Nat Tabris <nat.tabris@gmail.com>
2020-07-30 16:22:29 -0600

    Add docstring and test.

--- sleap/version.py
+++ sleap/version.py
@@ -1,1 +1,13 @@
+"""
+Current version of SLEAP package.
+
+This is used to set sleap.__version__, and it is also read by `setup.py` to set
+the version for the package (i.e., the version on PyPI).
+
+The version should be manually updated before creating a new release, and then
+the release should be created with a tag matching the version number set here.
+For example, if you set the version to X.Y.Z, then the tag should be "vX.Y.Z".
+
+Must be a semver string, "aN" should be appended for alpha releases.
+"""
 __version__ = "1.0.8"
