178b513216ff10266aad638cbb04e5c8705c22e5
Talmo Pereira <talmo@princeton.edu>
2020-08-13 22:50:35 -0400

    Specify tensor shapes for variable length values in LabelsProvider - Needed in TF 2.3 for ragged batching

--- sleap/nn/data/providers.py
+++ sleap/nn/data/providers.py
@@ -167,6 +167,8 @@ class LabelsReader:
                 [ind],
                 [image_dtype, tf.int32, tf.float32, tf.int32, tf.int64, tf.int32],
             )
+            instances = tf.ensure_shape(instances, tf.TensorShape([None, None, 2]))
+            skeleton_inds = tf.ensure_shape(skeleton_inds, tf.TensorShape([None]))
 
             return {
                 "image": image,
