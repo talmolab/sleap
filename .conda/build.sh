5f424b76d275da1bad053fe9affe58828a36acd6
Talmo Pereira <talmo@princeton.edu>
2020-12-04 16:34:26 -0500

    Test package build with new sleap/tensorflow package - Website workflow now triggers only on change to readme or docs/* - Update environment files

--- .conda/build.sh
+++ .conda/build.sh
@@ -11,15 +11,15 @@ export PIP_NO_INDEX=False
 export PIP_NO_DEPENDENCIES=False
 export PIP_IGNORE_INSTALLED=False
 
-pip install "numpy>=1.18.1,<1.19.0"
+# pip install "numpy>=1.18.1,<1.19.0"
 pip install attrs==19.3
 pip install cattrs==1.0.0rc
 pip install jsonpickle==1.2
 pip install networkx
-pip install tensorflow==2.3.1
-pip install h5py==2.10.0
+# pip install tensorflow==2.3.1
+# pip install h5py==2.10.0
 pip install python-rapidjson
-pip install opencv-python-headless==4.2.0.34
+# pip install opencv-python-headless==4.2.0.34
 pip install pandas
 pip install psutil
 pip install "PySide2>=5.12.0,<=5.14.1"
