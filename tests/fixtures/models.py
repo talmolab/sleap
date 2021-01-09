34d7db6ae952c293b51ed87cd263aae843f8fbd5
Talmo Pereira <talmo@princeton.edu>
2021-01-08 20:19:43 -0500

    Add minimal trained models as fixtures for tests

---
+++ tests/fixtures/models.py
@@ -0,0 +1,17 @@
+import sleap
+import pytest
+
+
+@pytest.fixture
+def min_centroid_model_path():
+    return "tests/data/models/minimal_instance.UNet.centroid"
+
+
+@pytest.fixture
+def min_centered_instance_model_path():
+    return "tests/data/models/minimal_instance.UNet.centered_instance"
+
+
+@pytest.fixture
+def min_bottomup_model_path():
+    return "tests/data/models/minimal_instance.UNet.bottomup"
