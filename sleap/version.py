83201d633ef76133e4196922f737457dbeab56e3
Talmo Pereira <talmo@princeton.edu>
2021-01-09 20:03:39 -0500

    Bump prerelease version

--- sleap/version.py
+++ sleap/version.py
@@ -10,4 +10,4 @@ For example, if you set the version to X.Y.Z, then the tag should be "vX.Y.Z".
 
 Must be a semver string, "aN" should be appended for alpha releases.
 """
-__version__ = "1.1.0a7"
+__version__ = "1.1.0a8"
