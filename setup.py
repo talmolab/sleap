7690ecc06fa3cbc30fcbbba5545ab7b2b510cd06
Talmo Pereira <talmo@princeton.edu>
2021-02-03 12:52:15 -0500

    Version printer - Adjust version detection in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 # Get the sleap version
 with open(path.join(here, "sleap/version.py")) as f:
     version_file = f.read()
-    sleap_version = re.search("\d.+(?=['\"])", version_file).group(0)
+    sleap_version = re.search("__version__ = \"([0-9\\.a]+)\"", version_file).group(1)
 
 
 def get_requirements(require_name=None):
--- sleap/__init__.py
+++ sleap/__init__.py
@@ -15,4 +15,4 @@ from sleap.nn import inference
 from sleap.nn.inference import load_model
 from sleap.nn.system import use_cpu_only, disable_preallocation
 from sleap.nn.system import summary as system_summary
-from sleap.version import __version__
+from sleap.version import __version__, versions
