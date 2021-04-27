ed91e6f08b6ad9c28d297f61195001bdfb4e978e
Arie Matsliah <arie@princeton.edu>
2021-04-27 13:02:34 -0700

    input states

--- sleap/gui/activities/inference/view.py
+++ sleap/gui/activities/inference/view.py
@@ -16,21 +16,45 @@ class InferenceActivity(QMainWindow):
     def __init__(self, parent: Optional[QObject], ctrl: InferenceGuiController):
         super().__init__(parent)
         self.controller = ctrl
+        self.input_widgets = InferenceActivityInputWidgets()
 
         self.title = "Inference"
         self.setWindowTitle(self.title)
 
-        self.table_widget = InferenceConfigWidget(self)
-        self.setCentralWidget(self.table_widget)
+        self.central_widget = InferenceActivityCentralWidget(self)
+        self.setCentralWidget(self.central_widget)
 
         self.setMinimumWidth(800)
 
         self.show()
 
 
-class InferenceConfigWidget(QWidget):
+class InferenceActivityInputWidgets(object):
+    __slots__ = [
+        # models tab
+        'model_type',
+        'single_instance_model',
+        'bottom_up_model',
+        'top_down_centroid_model',
+        'top_down_centered_instance_model',
+        # videos tab
+        'videos_table',
+        # instances tab
+        'max_num_instances_in_frame',
+        'enable_tracking',
+        'tracking_method',
+        'tracking_window_size',
+        # output tab
+        'output_dir_path',
+        'output_file_name',
+        'include_empty_frames',
+        'verbosity',
+    ]
+
+
+class InferenceActivityCentralWidget(QWidget):
     def __init__(self, parent):
-        super(InferenceConfigWidget, self).__init__(parent)
+        super(InferenceActivityCentralWidget, self).__init__(parent)
         self.layout = QVBoxLayout()
 
         # Tabs screen
@@ -49,6 +73,10 @@ class InferenceConfigWidget(QWidget):
     @property
     def controller(self) -> InferenceGuiController:
         return self.parent().controller
+
+    @property
+    def input_widgets(self) -> InferenceActivityInputWidgets:
+        return self.parent().input_widgets
 
     def build_action_buttons_widget(self):
         action_buttons = QWidget(self)
@@ -92,6 +120,7 @@ class InferenceConfigWidget(QWidget):
             table_model=self.controller.get_video_table_model()
         )
         tab.layout.addWidget(tab.videos_widget)
+        self.input_widgets.videos_table = tab.videos_widget
         tab.setLayout(tab.layout)
         tabs.addTab(tab, "Videos")
 
@@ -114,18 +143,27 @@ class InferenceConfigWidget(QWidget):
         output_box_layout = QFormLayout(self)
         output_box.setLayout(output_box_layout)
 
-        self.add_file_browser_row(output_box_layout, "Output file", controller.log)
+        # output dir and file name
+        self.input_widgets.output_dir_path = self.add_file_browser_row(
+            output_box_layout, "Output dir", controller.log)
+        output_file_name = QLineEdit()
+        output_box_layout.addRow("Output file name", output_file_name)
+        self.input_widgets.output_file_name = output_file_name
 
+        # include empty frames
         empty_frames = QCheckBox()
         empty_frames.setToolTip(
             "Include frames with no detected instances in the saved output file."
         )
         output_box_layout.addRow("Include empty frames  ", empty_frames)
+        self.input_widgets.include_empty_frames = empty_frames
 
+        # verbosity
         verbosity_widget = QComboBox()
         verbosity_widget.addItems(self.controller.get_verbosity_names())
         verbosity_widget.setMaximumWidth(150)
         output_box_layout.addRow("Log format / verbosity", verbosity_widget)
+        self.input_widgets.verbosity = verbosity_widget
 
         layout.addWidget(output_box)
 
@@ -156,27 +194,35 @@ class InferenceConfigWidget(QWidget):
         model_type_layout = QFormLayout()
         model_type_box.setLayout(model_type_layout)
 
+        # model type
         model_type_widget = QComboBox()
         model_type_widget.addItems(self.controller.get_model_type_names())
         model_type_widget.setMaximumWidth(250)
         model_type_layout.addRow("Type", model_type_widget)
+        self.input_widgets.model_type = model_type_widget
 
-        self.add_file_browser_row(model_type_layout, "Single Instance Model", self.controller.set_single_instance_model_path)
-        self.add_file_browser_row(model_type_layout, "Bottom Up model", self.controller.set_single_instance_model_path)
-        self.add_file_browser_row(model_type_layout, "Top Down Centroid model", self.controller.set_single_instance_model_path)
-        self.add_file_browser_row(model_type_layout, "Top Down Centered Instance model", self.controller.set_single_instance_model_path)
+        # model dirs
+        self.input_widgets.single_instance_model = self.add_file_browser_row(
+            model_type_layout, "Single Instance Model", self.controller.set_single_instance_model_path)
+        self.input_widgets.bottom_up_model = self.add_file_browser_row(
+            model_type_layout, "Bottom Up model", self.controller.set_single_instance_model_path)
+        self.input_widgets.top_down_centroid_model = self.add_file_browser_row(
+            model_type_layout, "Top Down Centroid model", self.controller.set_single_instance_model_path)
+        self.input_widgets.top_down_centered_instance_model = self.add_file_browser_row(
+            model_type_layout, "Top Down Centered Instance model", self.controller.set_single_instance_model_path)
 
         layout.addWidget(model_type_box)
 
-    def add_file_browser_row(self, layout: QLayout, caption: Text, path_setter: callable):
+    def add_file_browser_row(self, layout: QLayout, caption: Text, path_setter: callable) -> QLineEdit:
         widget = QHBoxLayout()
         path_text = QLineEdit()
         widget.addWidget(path_text)
 
+        path_text.textChanged.connect(path_setter(path_text.text()))
+
         def browse():
             path = FileDialog.openDir(None, dir=None, caption="Select model folder...")
             path_text.setText(path)
-            path_setter(path)
 
         browse_button = QPushButton("Browse..")
 
@@ -185,6 +231,7 @@ class InferenceConfigWidget(QWidget):
         )
         widget.addWidget(browse_button)
         layout.addRow(caption, widget)
+        return path_text
 
     def add_num_instances_box(self, tab):
         num_instances_box = QGroupBox("Animal instances")
@@ -196,8 +243,8 @@ class InferenceConfigWidget(QWidget):
         num_instances_widget.setValue(2)
         num_instances_widget.setMaximumWidth(50)
         num_instances.addRow("Max number of instances in frame", num_instances_widget)
-
         tab.layout.addWidget(num_instances_box)
+        self.input_widgets.max_num_instances_in_frame = num_instances_widget
 
     def add_tracking_box(self, tab):
         tracking_box = QGroupBox("Instance tracking")
@@ -211,14 +258,19 @@ class InferenceConfigWidget(QWidget):
         ql.setWordWrap(True)
         tracking.addRow("What?", ql)
 
+        # enable tracking
         enable_tracking = QCheckBox()
         tracking.addRow("Enable tracking", enable_tracking)
+        self.input_widgets.enable_tracking = enable_tracking
 
+        # tracking method
         tracking_method_widget = QComboBox()
         tracking_method_widget.addItems(self.controller.get_tracking_method_names())
         tracking_method_widget.setMaximumWidth(150)
         tracking.addRow("Tracking method", tracking_method_widget)
+        self.input_widgets.tracking_method = tracking_method_widget
 
+        # tracking window
         tracking_window_widget = QSpinBox()
         tracking_window_widget.setRange(0, 1000)
         tracking_window_widget.setValue(5)
@@ -227,6 +279,7 @@ class InferenceConfigWidget(QWidget):
         )
         tracking_window_widget.setMaximumWidth(50)
         tracking.addRow("Window size", tracking_window_widget)
+        self.input_widgets.tracking_window_size = tracking_window_widget
 
         tab.layout.addWidget(tracking_box)
 
