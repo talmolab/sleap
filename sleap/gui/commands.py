b13727fc6afa558fffb3f746dba408587830a7cf
David Samy <davidasamy@gmail.com>
2022-06-13 11:00:42 -0700

    Add support for edge cases: generating suggestions

--- sleap/gui/commands.py
+++ sleap/gui/commands.py
@@ -2119,18 +2119,23 @@ class GenerateSuggestions(EditCommand):
             "Generating list of suggested frames... " "This may take a few minutes.",
             context.app,
         )
-
-        if (
-            params["target"] == "current video"
-        ):  # checks if current video is selected in gui
-            params["videos"] = [context.state["video"]]
+        if len(context.labels.videos) == 0:
+            print("Error: no videos to generate suggestions for")
         else:
-            params["videos"] = context.labels.videos
-        new_suggestions = VideoFrameSuggestions.suggest(
-            labels=context.labels, params=params
-        )
+            if (
+                params["target"] == "current video"
+            ):  # checks if current video is selected in gui
+                if context.state["video"] is not None:
+                    params["videos"] = [context.state["video"]]
+                else:
+                    params["videos"] = [context.labels.videos[0]]
+            else:
+                params["videos"] = context.labels.videos
 
-        context.labels.set_suggestions(new_suggestions)
+            new_suggestions = VideoFrameSuggestions.suggest(
+                labels=context.labels, params=params
+            )
+            context.labels.set_suggestions(new_suggestions)
 
         win.hide()
 
