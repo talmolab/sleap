938106a71432a1af68509cc298a079ca96c1809b
roomrys <38435167+roomrys@users.noreply.github.com>
2022-12-08 10:16:31 -0800

    Add line coverage

--- tests/nn/test_tracker_components.py
+++ tests/nn/test_tracker_components.py
@@ -14,16 +14,33 @@ from sleap.instance import PredictedInstance
 from sleap.skeleton import Skeleton
 
 
+def tracker_by_name(**kwargs):
+    t = Tracker.make_tracker_by_name(**kwargs)
+    t.track([])
+    t.final_pass([])
+
+
 @pytest.mark.parametrize("tracker", ["simple", "flow"])
 @pytest.mark.parametrize("similarity", ["instance", "iou", "centroid"])
 @pytest.mark.parametrize("match", ["greedy", "hungarian"])
 @pytest.mark.parametrize("count", [0, 2])
 def test_tracker_by_name(tracker, similarity, match, count):
-    t = Tracker.make_tracker_by_name(
-        "flow", "instance", "greedy", clean_instance_count=2
+    tracker_by_name(
+        tracker=tracker, similarity=similarity, match=match, clean_instance_count=count
     )
-    t.track([])
-    t.final_pass([])
+
+
+@pytest.mark.parametrize("tracker", ["simple", "flow"])
+@pytest.mark.parametrize("oks_score_weighting", ["True", "False"])
+@pytest.mark.parametrize("oks_normalization", ["all", "ref", "union"])
+def test_oks_tracker_by_name(tracker, oks_score_weighting, oks_normalization):
+    tracker_by_name(
+        tracker=tracker,
+        similarity="object_keypoint",
+        matching="greedy",
+        oks_score_weighting=oks_score_weighting,
+        oks_normalization=oks_normalization,
+    )
 
 
 def test_cull_instances(centered_pair_predictions):
