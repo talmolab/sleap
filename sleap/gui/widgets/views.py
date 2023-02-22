76d5fefd4dd143db9c929423af4901acfe552ec7
roomrys <38435167+roomrys@users.noreply.github.com>
2023-02-22 14:56:09 -0800

    Final? clean-up for collapsible widget

--- sleap/gui/widgets/views.py
+++ sleap/gui/widgets/views.py
@@ -1,5 +1,6 @@
 """GUI code for the views (e.g. Videos, Skeleton, Labeling Suggestions, etc.)."""
 
+from typing import Tuple
 from qtpy.QtWidgets import (
     QWidget,
     QHBoxLayout,
@@ -9,9 +10,10 @@ from qtpy.QtWidgets import (
     QSizePolicy,
 )
 from qtpy.QtCore import Qt
+from qtpy.QtGui import QCursor
 
 
-class CollapsibleWidget(QFrame):
+class CollapsibleWidget(QWidget):
     """An animated collapsible QWidget.
 
     Derived from: https://stackoverflow.com/a/37119983/13281260
@@ -19,20 +21,38 @@ class CollapsibleWidget(QFrame):
 
     def __init__(self, title: str, parent: QWidget = None):
         super().__init__(parent=parent)
-        self.setObjectName("container")
-        self.setFrameShape(QFrame.NoFrame)
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+
+        # Create the header widget which contains the toggle button.
+        self.header_widget, self.toggle_button = self.create_header_widget(title)
+
+        # Content area for setting an external layout to.
+        self.content_area = QWidget()
+
+        # Tie everything together in a main layout.
+        main_layout = self.create_main_layout()
+        self.setLayout(main_layout)
+
+    def create_toggle_button(self, title="") -> QToolButton:
+        """Create our custom toggle button."""
+
+        toggle_button = QToolButton()
+        toggle_button.setStyleSheet("QToolButton { border: none; }")
+        toggle_button.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        toggle_button.setArrowType(Qt.ArrowType.RightArrow)
+        toggle_button.setText(title)
+        toggle_button.setCheckable(True)
+        toggle_button.setChecked(False)
+        toggle_button.setCursor(QCursor(Qt.PointingHandCursor))
+
+        toggle_button.clicked.connect(self.toggle_button_callback)
+
+        return toggle_button
+
+    def create_header_widget(self, title="") -> Tuple[QWidget, QToolButton]:
+        """Create header widget which includes `QToolButton` and `QFrame`."""
 
         # Create our custom toggle button.
-        self.toggle_button = QToolButton()
-        self.toggle_button.setStyleSheet("QToolButton { border: none; }")
-        self.toggle_button.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
-        self.toggle_button.setArrowType(Qt.ArrowType.RightArrow)
-        self.toggle_button.setText(title)
-        self.toggle_button.setCheckable(True)
-        self.toggle_button.setChecked(False)
-
-        self.toggle_button.clicked.connect(self.toggle_button_callback)
+        toggle_button = self.create_toggle_button(title)
 
         # Create the header line.
         header_line = QFrame()
@@ -43,7 +63,7 @@ class CollapsibleWidget(QFrame):
 
         # Created the layout for the header.
         header_layout = QHBoxLayout()
-        header_layout.addWidget(self.toggle_button)
+        header_layout.addWidget(toggle_button)
         header_layout.addWidget(header_line)
         header_layout.setContentsMargins(0, 0, 0, 0)
 
@@ -51,15 +71,17 @@ class CollapsibleWidget(QFrame):
         header_widget = QWidget()
         header_widget.setLayout(header_layout)
 
-        # Content area for setting an external layout to.
-        self.content_area = QWidget()
+        return header_widget, toggle_button
 
-        # Tie everything together in a main layout.
+    def create_main_layout(self) -> QVBoxLayout:
+        """Tie everything together in a main layout."""
+
         main_layout = QVBoxLayout()
-        main_layout.addWidget(header_widget)
+        main_layout.addWidget(self.header_widget)
         main_layout.addWidget(self.content_area)
         main_layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(main_layout)
+
+        return main_layout
 
     def toggle_button_callback(self, checked: bool):
         self.toggle_button.setArrowType(
