347d4bb3c62a545904e61c0550391ed9cae51316
Eric Leonardis <eleonardis@salk.edu>
2023-04-13 16:14:34 -0700

    tests masked none, single and multiple GPUs

--- tests/nn/test_system.py
+++ tests/nn/test_system.py
@@ -5,8 +5,37 @@ be available.
 """
 
 from sleap.nn.system import get_gpu_memory
+import os
+import pytest
 
 
 def test_get_gpu_memory():
     # Make sure this doesn't throw an exception
     memory = get_gpu_memory()
+
+
+def test_get_gpu_memory_visible():
+    cuda_visible_devices = os.environ.get("CUDA_VISIBLE_DEVICES", None)
+
+    if cuda_visible_devices is None:
+        pytest.skip("CUDA_VISIBLE_DEVICES not set.")
+
+    elif cuda_visible_devices == "0":
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+        gpu_memory = get_gpu_memory()
+
+        assert len(gpu_memory) > 0
+        assert len(gpu_memory) == 1
+
+        os.environ.pop("CUDA_VISIBLE_DEVICES", None)
+
+    elif "," in cuda_visible_devices:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+
+        gpu_memory = get_gpu_memory()
+
+        assert len(gpu_memory) > 0
+        assert len(gpu_memory) == len(cuda_visible_devices.split(","))
+
+        os.environ.pop("CUDA_VISIBLE_DEVICES", None)
