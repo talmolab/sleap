271126921e611a3a2552422705a611a7732e68c0
roomrys <38435167+roomrys@users.noreply.github.com>
2023-05-04 15:53:34 -0700

    Add tracking_score attribute to `Instance`s

--- tests/io/test_dataset.py
+++ tests/io/test_dataset.py
@@ -8,7 +8,6 @@ from sleap.skeleton import Skeleton
 from sleap.instance import Instance, Point, LabeledFrame, PredictedInstance, Track
 from sleap.io.video import Video, MediaVideo
 from sleap.io.dataset import Labels, load_file
-from sleap.io.legacy import load_labels_json_old
 from sleap.io.format.ndx_pose import NDXPoseAdaptor
 from sleap.io.format import filehandle
 from sleap.gui.suggestions import VideoFrameSuggestions, SuggestionFrame
@@ -744,6 +743,36 @@ def test_dont_unify_skeletons():
 
     # Make sure we can serialize this
     labels.to_dict()
+
+
+def test_instance_cattr(centered_pair_predictions: Labels, tmpdir: str):
+    labels = centered_pair_predictions
+    lf = labels.labeled_frames[0]
+    pred_inst: PredictedInstance = lf[0]
+    skeleton = pred_inst.skeleton
+    track = pred_inst.track
+
+    # Initialize Instance
+    instance = Instance.from_pointsarray(
+        points=pred_inst.numpy(), skeleton=skeleton, track=track
+    )
+    instance.from_predicted = pred_inst
+    assert instance.tracking_score is None
+    labels.add_instance(lf, instance)
+
+    pred_tracking_score = pred_inst.tracking_score
+    inst_tracking_score = instance.tracking_score
+
+    filename = str(PurePath(tmpdir, "labels.slp"))
+    labels.save(filename)
+
+    labels_loaded = sleap.load_file(filename)
+    lf_loaded = labels_loaded.labeled_frames[0]
+    pred_inst_loaded = lf_loaded.predicted_instances[0]
+    instance_loaded = lf_loaded.user_instances[0]
+
+    assert pred_inst_loaded.tracking_score == pred_tracking_score
+    assert instance_loaded.tracking_score == inst_tracking_score
 
 
 def test_instance_access():
