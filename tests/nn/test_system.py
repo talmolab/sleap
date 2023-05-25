f4a7f455f52b8487670724cb0726deb460ea4e12
Eric Leonardis <leonardiseric@gmail.com>
2023-05-25 11:57:26 -0700

    add test for gpu order and length in nvidia and tf

--- tests/nn/test_system.py
+++ tests/nn/test_system.py
@@ -5,39 +5,44 @@ be available.
 """
 
 from sleap.nn.system import get_gpu_memory
+from sleap.nn.system import get_all_gpus
 import os
 import pytest
+import subprocess
+import tensorflow as tf
 
 
 def test_get_gpu_memory():
     # Make sure this doesn't throw an exception
     memory = get_gpu_memory()
 
+@pytest.mark.parametrize("cuda_visible_devices", ["0", "1", "0,1"])
+def test_get_gpu_memory_visible(cuda_visible_devices):
 
-def test_get_gpu_memory_visible():
+    os.environ["CUDA_VISIBLE_DEVICES"] = cuda_visible_devices
 
-    cuda_visible_devices = os.environ.get("CUDA_VISIBLE_DEVICES", None)
+    gpu_memory = get_gpu_memory()
 
     if cuda_visible_devices is None or cuda_visible_devices is []:
         pytest.skip("CUDA_VISIBLE_DEVICES not set.")
 
-    elif cuda_visible_devices == "0":
-        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-        gpu_memory = get_gpu_memory()
-
+    elif cuda_visible_devices == "0" or cuda_visible_devices == "1":
         assert len(gpu_memory) > 0
         assert len(gpu_memory) == 1
 
-        os.environ.pop("CUDA_VISIBLE_DEVICES", None)
-
     elif cuda_visible_devices == "0,1":
-
-        os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
-
-        gpu_memory = get_gpu_memory()
-
         assert len(gpu_memory) > 0
         assert len(gpu_memory) == 2
 
-        os.environ.pop("CUDA_VISIBLE_DEVICES", None)
+def test_gpu_order_and_length():
+
+    # Get GPU indices from sleap.nn.system.get_all_gpus
+    sleap_indices = [int(gpu.name.split(':')[-1]) for gpu in get_all_gpus()]
+    
+    # Get GPU indices from nvidia-smi
+    command = ["nvidia-smi", "--query-gpu=index", "--format=csv,noheader"]
+    nvidia_indices = subprocess.check_output(command).decode('utf-8').strip().split('\n')
+    nvidia_indices = [int(idx) for idx in nvidia_indices]
+
+    # Assert that the order and length of GPU indices match
+    assert sleap_indices == nvidia_indices
