2aa466c18f1eec9ce3b65d32ab50bf1b445d776b
Eric Leonardis <leonardiseric@gmail.com>
2023-05-25 18:28:38 -0700

    fix linting with Black

--- tests/nn/test_system.py
+++ tests/nn/test_system.py
@@ -16,11 +16,14 @@ def test_get_gpu_memory():
     # Make sure this doesn't throw an exception
     memory = get_gpu_memory()
 
+
 @pytest.mark.parametrize("cuda_visible_devices", ["0", "1", "0,1"])
 def test_get_gpu_memory_visible(cuda_visible_devices):
     # Get GPU indices from nvidia-smi
     command = ["nvidia-smi", "--query-gpu=index", "--format=csv,noheader"]
-    nvidia_indices = subprocess.check_output(command).decode('utf-8').strip().split('\n')
+    nvidia_indices = (
+        subprocess.check_output(command).decode("utf-8").strip().split("\n")
+    )
 
     os.environ["CUDA_VISIBLE_DEVICES"] = cuda_visible_devices
 
@@ -37,13 +40,16 @@ def test_get_gpu_memory_visible(cuda_visible_devices):
         assert len(gpu_memory) > 0
         assert len(gpu_memory) == 2
 
+
 def test_gpu_order_and_length():
     # Get GPU indices from sleap.nn.system.get_all_gpus
-    sleap_indices = [int(gpu.name.split(':')[-1]) for gpu in get_all_gpus()]
-    
+    sleap_indices = [int(gpu.name.split(":")[-1]) for gpu in get_all_gpus()]
+
     # Get GPU indices from nvidia-smi
     command = ["nvidia-smi", "--query-gpu=index", "--format=csv,noheader"]
-    nvidia_indices = subprocess.check_output(command).decode('utf-8').strip().split('\n')
+    nvidia_indices = (
+        subprocess.check_output(command).decode("utf-8").strip().split("\n")
+    )
     nvidia_indices = [int(idx) for idx in nvidia_indices]
 
     # Assert that the order and length of GPU indices match
