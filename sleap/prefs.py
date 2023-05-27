f38268589339d753f912826675ae03adcdd6072b
Liezl Maree <38435167+roomrys@users.noreply.github.com>
2023-05-26 17:08:20 -0700

    Add caching as a preference and menu checkbox

--- sleap/prefs.py
+++ sleap/prefs.py
@@ -15,6 +15,7 @@ class Preferences(object):
         "medium step size": 10,
         "large step size": 100,
         "color predicted": False,
+        "cache singleimagevideos": False,
         "propagate track labels": True,
         "palette": "standard",
         "bold lines": False,
