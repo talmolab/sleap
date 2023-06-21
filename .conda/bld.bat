eb26734b3d9f329ff966c7f2be125912f3891274
roomrys <lmaree@ucsd.edu>
2023-06-20 18:18:06 -0700

    Ubuntu build and environments working

--- .conda/bld.bat
+++ .conda/bld.bat
@@ -1,10 +1,9 @@
-@REM @echo off
+@REM Install anything that didn't get conda installed via pip.
 
+@REM We need to turn pip index back on because Anaconda turns it off for some reason.
 set PIP_NO_INDEX=False
 set PIP_NO_DEPENDENCIES=False
 set PIP_IGNORE_INSTALLED=False
-
-echo Running bld.bat
 
 @REM Install the pip dependencies. Note: Using urls to wheels might be better: 
 @REM https://docs.conda.io/projects/conda-build/en/stable/user-guide/wheel-files.html)
