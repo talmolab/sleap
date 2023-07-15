Minor fix in computation of OKS (#1383)
* fix compute oks

* Update the oks fix

sleap/nn/evals.py
@@ -203,7 +203,7 @@ def compute_oks(
     assert distance.shape == (n_gt, n_pr, n_nodes)
 
     # Compute the normalization factor per keypoint.
-    spread_factor = (2 * stddev) ** 2
+    spread_factor = stddev ** 2
     scale_factor = 2 * (scale + np.spacing(1))
     normalization_factor = np.reshape(spread_factor, (1, 1, n_nodes)) * np.reshape(
         scale_factor, (n_gt, 1, 1)