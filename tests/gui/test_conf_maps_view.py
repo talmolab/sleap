c4dd30d69f36e6cf178688d25e528ab5f02a7ada
roomrys <38435167+roomrys@users.noreply.github.com>
2023-08-30 13:37:40 -0700

    Move imports inside test function

--- tests/gui/test_conf_maps_view.py
+++ tests/gui/test_conf_maps_view.py
@@ -1,11 +1,11 @@
 import pytest
 
-from sleap.gui.widgets.video import QtVideoPlayer
-from sleap.gui.overlays.confmaps import ConfMapsPlot
-
 
 @pytest.mark.exclude_from_linux_pip_test
 def test_gui_conf_maps(qtbot, hdf5_confmaps):
+
+    from sleap.gui.widgets.video import QtVideoPlayer
+    from sleap.gui.overlays.confmaps import ConfMapsPlot
 
     vp = QtVideoPlayer()
     vp.show()
