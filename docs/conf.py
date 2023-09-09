232400ed296b8ed66ddc821063c95af0eba9a477
roomrys <>
2023-09-09 07:54:44 -0700

    Internal import after adding relative path to sys

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,10 @@
 import os
 import sys
 import shutil
-import docs.utils
 from datetime import date
 
 sys.path.insert(0, os.path.abspath(".."))
+import docs.utils
 
 
 # -- Project information -----------------------------------------------------
