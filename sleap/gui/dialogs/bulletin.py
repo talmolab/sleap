b7f77488707d29adad9435a76e57738fe3d698fc
Shrivaths Shyam <shrivaths1998@gmail.com>
2023-10-16 17:46:24 -0700

    Adding the bulletin dialog box class

---
+++ sleap/gui/dialogs/bulletin.py
@@ -0,0 +1,30 @@
+"""
+GUI for displaying the new announcement.
+"""
+
+from typing import List
+from qtpy import QtWidgets
+
+
+class BulletinDialog(QtWidgets.QDialog): 
+    """ 
+    Dialog window to display the announcement. 
+    """ 
+
+    app: "MainWindow"
+
+    def __init__(self, *args, **kwargs): 
+        super(BulletinDialog, self).__init__(*args, **kwargs) 
+
+        self.setWindowTitle("Announcement")
+        self.info_msg() 
+
+    def info_msg(self): 
+        """Display information about changes.""" 
+        msg = QtWidgets.QMessageBox() 
+        information = self.app.state["announcement"]
+        msg.setText( 
+            self.app.state["announcement"]
+        ) 
+        msg.exec_()
+
