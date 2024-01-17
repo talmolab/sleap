c218b10958d1c7071b7e49b086096473166b0633
Shrivaths Shyam <shrivaths1998@gmail.com>
2024-01-17 13:51:29 -0800

    add regex for date check

--- docs/make_bulletin_json.py
+++ docs/make_bulletin_json.py
@@ -1,10 +1,12 @@
 import json
-import os
+import re
+from pathlib import Path
 
-# Set the file paths
-input_md_file = os.path.join(os.path.dirname(__file__), "bulletin.md")
-output_json_file = os.path.join(os.path.dirname(__file__), "bulletin.json")
+# Set the file path to the markdown file
+input_md_file = Path(__file__).resolve().parent / "bulletin.md"
 
+# Regex for date
+date_pattern = r'^_(\d{2}/\d{2}/\d{4})_$'
 
 def generate_json_file():
     with open(input_md_file, "r", encoding="utf-8") as md_file:
@@ -28,7 +30,7 @@ def generate_json_file():
             date = "DEFAULT_DATE"
         elif line.startswith("## "):
             title = line[3:].strip()
-        elif line.startswith("_"):
+        elif re.match(date_pattern, line):
             date = line[1 : len(line) - 1].strip()
         else:
             content += line + "\n"
