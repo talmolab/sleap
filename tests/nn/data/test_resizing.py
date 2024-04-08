3e7f03d3bd422891da20c21cb4285bc2db2c323a
Talmo Pereira <talmo@salk.edu>
2024-04-07 22:26:12 -0700

    Lint

--- tests/nn/data/test_resizing.py
+++ tests/nn/data/test_resizing.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 import sleap
 from sleap.nn.system import use_cpu_only
+
 # use_cpu_only()  # hide GPUs for test
 from sleap.nn.data import resizing
 from sleap.nn.data import providers
