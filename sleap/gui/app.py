d2f522b811303ad67c48d40b70cf29a3aed7a816
roomrys <38435167+roomrys@users.noreply.github.com>
2024-05-29 15:18:22 -0700

    Remove unused code

--- sleap/gui/app.py
+++ sleap/gui/app.py
@@ -1849,7 +1849,3 @@ def main(args: Optional[list] = None, labels: Optional[Labels] = None):
         app.exec_()
 
     pass
-
-if __name__ == "__main__":
-    ds = os.environ["ds-mview"]
-    main([ds])
