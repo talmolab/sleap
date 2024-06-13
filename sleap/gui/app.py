f9ff64a1c07cd2c4588236d7370ba17266347f57
roomrys <38435167+roomrys@users.noreply.github.com>
2024-06-13 16:39:50 -0700

    Remove debug block

--- sleap/gui/app.py
+++ sleap/gui/app.py
@@ -1875,8 +1875,3 @@ def main(args: Optional[list] = None, labels: Optional[Labels] = None):
         app.exec_()
 
     pass
-
-
-# if __name__ == "__main__":
-#     ds = os.environ["dsmview"]
-#     main([ds])
