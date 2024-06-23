1e4361866cbbf99815a50d8f7dbe47f6f219f2b5
Andrew Park <hep003@ucsd.edu>
2024-06-23 15:01:05 -0700

    deleted unnecessary code

--- sleap/gui/app.py
+++ sleap/gui/app.py
@@ -1872,8 +1872,3 @@ def main(args: Optional[list] = None, labels: Optional[Labels] = None):
         app.exec_()
 
     pass
-
-if __name__ == "__main__":
-    ds = "tests/data/cameras/minimal_session/min_session_frame_groups.slp"
-    # ds = os.environ["dsmview"]
-    main([ds])
