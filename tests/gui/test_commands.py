e9239ef2b24aca7b884708c3414a217eda896914
roomrys <38435167+roomrys@users.noreply.github.com>
2024-07-01 13:16:06 -0700

    Add minimal test

--- tests/gui/test_commands.py
+++ tests/gui/test_commands.py
@@ -1451,3 +1451,19 @@ def test_DeleteInstanceGroup(multiview_min_session_frame_groups: Labels):
     # Check InstanceGroup.instances
     assert len(instance_group_0.instances) == 8
     assert len(instance_group_1.instances) == 6
+
+
+def test_setInstanceGroupName(multiview_min_session_frame_groups):
+    labels: Labels = multiview_min_session_frame_groups
+    session: RecordingSession = labels.sessions[0]
+
+    # Set-up CommandContext
+    context: CommandContext = CommandContext.from_labels(labels)
+    context.state["session"] = session
+
+    # Start test
+    frame_group = session.frame_groups[0]
+    instance_group: InstanceGroup = frame_group.instance_groups[0]
+    new_name = "New Name"
+    context.setInstanceGroupName(instance_group=instance_group, name=new_name)
+    assert instance_group.name == new_name
