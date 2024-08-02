5649a30ffd0c9c33d668ee3b69e03ca2a8e4f3d4
Elizabeth Berrigan <berri104@gmail.com>
2024-08-02 16:55:02 -0700

    remove `export_clip` changes

--- sleap/gui/dialogs/export_clip.py
+++ sleap/gui/dialogs/export_clip.py
@@ -11,16 +11,16 @@ class ExportClipDialog(FormBuilderModalDialog):
 
         super().__init__(form_name="labeled_clip_form")
 
-        can_use_ffmpeg = VideoWriter.can_use_ffmpeg()
+        can_use_skvideo = VideoWriter.can_use_skvideo()
 
-        if can_use_ffmpeg:
+        if can_use_skvideo:
             message = (
                 "<i><b>MP4</b> file will be encoded using "
-                "system ffmpeg via imageio (preferred option).</i>"
+                "system ffmpeg via scikit-video (preferred option).</i>"
             )
         else:
             message = (
-                "<i>Unable to use ffpmeg via imageio. "
+                "<i>Unable to use ffpmeg via scikit-video. "
                 "<b>AVI</b> file will be encoding using OpenCV.</i>"
             )
 
