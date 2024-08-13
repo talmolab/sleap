a080f55fde827503a7fb335a111db9dd47ed96d4
Elizabeth Berrigan <berri104@gmail.com>
2024-08-13 11:34:07 -0700

    test `VideoWriterImageio` minimally

--- tests/io/test_videowriter.py
+++ tests/io/test_videowriter.py
@@ -1,5 +1,6 @@
 import os
-from sleap.io.videowriter import VideoWriter, VideoWriterOpenCV
+from pathlib import Path
+from sleap.io.videowriter import VideoWriter, VideoWriterOpenCV, VideoWriterImageio
 
 
 def test_video_writer(tmpdir, small_robot_mp4_vid):
@@ -38,3 +39,29 @@ def test_cv_video_writer(tmpdir, small_robot_mp4_vid):
     writer.close()
 
     assert os.path.exists(out_path)
+
+
+def test_imageio_video_writer(tmpdir, small_robot_mp4_vid):
+    out_path = Path(tmpdir) / "clip.avi"
+
+    # Make sure imageio video writer works
+    writer = VideoWriterImageio(
+        out_path,
+        height=small_robot_mp4_vid.height,
+        width=small_robot_mp4_vid.width,
+        fps=small_robot_mp4_vid.fps,
+    )
+
+    writer.add_frame(small_robot_mp4_vid[0][0])
+    writer.add_frame(small_robot_mp4_vid[1][0])
+
+    writer.close()
+
+    assert os.path.exists(out_path)
+    # Check attributes
+    assert writer.height == small_robot_mp4_vid.height
+    assert writer.width == small_robot_mp4_vid.width
+    assert writer.fps == small_robot_mp4_vid.fps
+    assert writer.filename == out_path
+    assert writer.crf == 21
+    assert writer.preset == "superfast"
