e4ba24d060cb55eac0622cfc0fbff101984fdb19
roomrys <38435167+roomrys@users.noreply.github.com>
2024-08-22 14:28:57 -0700

    Lint

--- sleap/gui/widgets/monitor.py
+++ sleap/gui/widgets/monitor.py
@@ -419,7 +419,9 @@ class LossViewer(QtWidgets.QMainWindow):
         self.ax.spines["right"].set_visible(False)
 
         # Set the tick markers color to light gray, but not the tick labels
-        self.ax.tick_params(axis="both", which="both", color="#d3d3d3", labelsize="small")
+        self.ax.tick_params(
+            axis="both", which="both", color="#d3d3d3", labelsize="small"
+        )
 
         # Add gridlines at the tick labels
         self.ax.grid(True, which="major", linewidth=0.5, color="#d3d3d3")
@@ -926,7 +928,7 @@ if __name__ == "__main__":
             which=which,
         )
 
-    T0 = 200
+    T0 = 2000
 
     timer_0 = QtCore.QTimer()
     timer_0.timeout.connect(lambda which="batch": test_point(which=which))
