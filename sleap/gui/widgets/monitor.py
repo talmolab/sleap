ec129e5548968c408d287b3980b5b9750ea06375
roomrys <38435167+roomrys@users.noreply.github.com>
2024-08-22 17:06:24 -0700

    Add class LossPlot

--- sleap/gui/widgets/monitor.py
+++ sleap/gui/widgets/monitor.py
@@ -21,6 +21,479 @@ from sleap.nn.config.training_job import TrainingJobConfig
 logger = logging.getLogger(__name__)
 
 
+class LossPlot(MplCanvas):
+    """Matplotlib canvas for diplaying training and validation loss curves."""
+
+    def __init__(
+        self,
+        width: int = 5,
+        height: int = 4,
+        dpi: int = 100,
+        log_scale: bool = True,
+        ignore_outliers: bool = False,
+    ):
+        super().__init__(width=width, height=height, dpi=dpi)
+
+        self._log_scale: bool = log_scale
+
+        self.ignore_outliers = ignore_outliers
+
+        # Initialize the series for the plot
+        self.series: dict = {}
+        COLOR_TRAIN = (18, 158, 220)
+        COLOR_VAL = (248, 167, 52)
+        COLOR_BEST_VAL = (151, 204, 89)
+
+        # Initialize scatter series for batch training loss
+        self.series["batch"] = self.init_series(
+            series_type=self.axes.scatter,
+            name="Batch Training Loss",
+            color=COLOR_TRAIN + (48,),
+            border_color=(255, 255, 255, 25),
+        )
+
+        # Initialize line series for epoch training loss
+        self.series["epoch_loss"] = self.init_series(
+            series_type=self.axes.plot,
+            name="Epoch Training Loss",
+            color=COLOR_TRAIN + (255,),
+            line_width=3.0,
+        )
+
+        # Initialize line series for epoch validation loss
+        self.series["val_loss"] = self.init_series(
+            series_type=self.axes.plot,
+            name="Epoch Validation Loss",
+            color=COLOR_VAL + (255,),
+            line_width=3.0,
+            zorder=4,  # Below best validation loss series
+        )
+
+        # Initialize scatter series for best epoch validation loss
+        self.series["val_loss_best"] = self.init_series(
+            series_type=self.axes.scatter,
+            name="Best Validation Loss",
+            color=COLOR_BEST_VAL + (255,),
+            border_color=(255, 255, 255, 25),
+            zorder=5,  # Above epoch validation loss series
+        )
+
+        # Set up the major gridlines
+        self._setup_major_gridlines()
+
+        # Set up the x-axis
+        self._setup_x_axis()
+
+        # Set up the y-axis
+        self._set_up_y_axis()
+
+        # Set up the legend
+        self._setup_legend()
+
+        # Adjust the figure layout
+        self.fig.subplots_adjust(top=0.71)  # Adjust the top parameters as needed
+
+    @property
+    def log_scale(self):
+        """Returns True if the plot has a log scale for y-axis."""
+
+        return self._log_scale
+
+    @log_scale.setter
+    def log_scale(self, val):
+        """Sets the scale of the y axis to log if True else linear."""
+
+        if isinstance(val, bool):
+            self._log_scale = val
+
+        y_scale = "log" if self._log_scale else "linear"
+        self.axes.set_yscale(y_scale)
+        self.redraw_plot()
+
+    def init_series(
+        self,
+        series_type,
+        color,
+        name: Optional[str] = None,
+        line_width: Optional[float] = None,
+        border_color: Optional[Tuple[int, int, int]] = None,
+        zorder: Optional[int] = None,
+    ):
+
+        # Set the color
+        color = [c / 255.0 for c in color]  # Normalize color values to [0, 1]
+
+        # Create the series
+        series = series_type(
+            [],
+            [],
+            color=color,
+            label=name,
+            marker="o",
+            zorder=zorder,
+        )
+
+        # ax.plot returns a list of PathCollections, so we need to get the first one
+        if not isinstance(series, PathCollection):
+            series = series[0]
+
+        if line_width is not None:
+            series.set_linewidth(line_width)
+
+        # Set the border color (edge color)
+        if border_color is not None:
+            border_color = [
+                c / 255.0 for c in border_color
+            ]  # Normalize color values to [0, 1]
+            series.set_edgecolor(border_color)
+
+        return series
+
+    def _setup_x_axis(self):
+        """Set up the x axis.
+
+        This includes setting the label, limits, and bottom/right adjustment.
+        """
+
+        self.axes.set_xlim(0, 1)
+        self.axes.set_xlabel("Batches", fontweight="bold", fontsize="small")
+        self.fig.subplots_adjust(bottom=0.14, right=0.97)
+
+    def _set_up_y_axis(self):
+        """Set up the y axis.
+
+        This includes setting the label, limits, scaling, and left adjustment.
+        """
+
+        # Set the minimum value of the y-axis depending on scaling
+        if self.log_scale:
+            yscale = "log"
+            y_min = 0.001
+        else:
+            yscale = "linear"
+            y_min = 0
+        self.axes.set_ylim(bottom=y_min)
+        self.axes.set_yscale(yscale)
+
+        # Set the y-label name, size, wight, and position
+        self.axes.set_ylabel("Loss", fontweight="bold", fontsize="small")
+        self.axes.yaxis.set_label_coords(-0.19, 0.5)
+
+        # Adjust the bottom of the plot
+        self.fig.subplots_adjust(left=0.2)
+
+    def _setup_legend(self):
+        """Set up the legend."""
+
+        # Move the legend outside the plot on the upper left
+        self.axes.legend(
+            loc="upper left", bbox_to_anchor=(-0.25, 1.5), fontsize="small"
+        )
+
+    def _setup_major_gridlines(self):
+
+        # Set the outline color of the plot to gray
+        for spine in self.axes.spines.values():
+            spine.set_edgecolor("#d3d3d3")  # Light gray color
+
+        # Remove the top and right axis spines
+        self.axes.spines["top"].set_visible(False)
+        self.axes.spines["right"].set_visible(False)
+
+        # Set the tick markers color to light gray, but not the tick labels
+        self.axes.tick_params(
+            axis="both", which="both", color="#d3d3d3", labelsize="small"
+        )
+
+        # Add gridlines at the tick labels
+        self.axes.grid(True, which="major", linewidth=0.5, color="#d3d3d3")
+
+    def _add_midpoint_gridlines(self):
+        # Clear existing minor vertical lines
+        for line in self.axes.get_lines():
+            if line.get_linestyle() == ":":
+                line.remove()
+
+        # Add gridlines at midpoint between major ticks
+        major_ticks = self.axes.yaxis.get_majorticklocs()
+        if len(major_ticks) > 1:
+            prev_major_tick = major_ticks[0]
+            for major_tick in major_ticks[:-1]:
+                midpoint = (major_tick + prev_major_tick) / 2
+                self.axes.axhline(midpoint, linestyle=":", linewidth=0.5, color="#d3d3d3")
+                prev_major_tick = major_tick
+
+    def set_data_on_scatter(self, xs, ys, which):
+        """Set data on a scatter plot.
+
+        Not to be used with line plots.
+
+        Args:
+            xs: The x-coordinates of the data points.
+            ys: The y-coordinates of the data points.
+            which: The type of data point. Possible values are:
+                * "batch"
+                * "val_loss_best"
+        """
+
+        offsets = np.column_stack((xs, ys))
+        self.series[which].set_offsets(offsets)
+
+    def add_data_to_plot(self, x, y, which):
+        """Add data to a line plot.
+
+        Not to be used with scatter plots.
+
+        Args:
+            x: The x-coordinate of the data point.
+            y: The y-coordinate of the data point.
+            which: The type of data point. Possible values are:
+                * "epoch_loss"
+                * "val_loss"
+        """
+
+        x_data, y_data = self.series[which].get_data()
+        self.series[which].set_data(np.append(x_data, x), np.append(y_data, y))
+
+    @staticmethod
+    def calculate_xlim(x: np.ndarray, dx: float = 0.5):
+        """Calculates x-axis limits.
+
+        Args:
+            x: Array of x data to fit the limits to.
+            dx: The padding to add to the limits.
+
+        Returns:
+            Tuple of the minimum and maximum x-axis limits.
+        """
+
+        x_min = min(x) - dx
+        x_min = x_min if x_min > 0 else 0
+        x_max = max(x) + dx
+
+        return x_min, x_max
+
+    def calculate_ylim(self, y: np.ndarray, dy: float = 0.02):
+        """Calculates y-axis limits.
+
+        Args:
+            y: Array of y data to fit the limits to.
+            dy: The padding to add to the limits.
+
+        Returns:
+            Tuple of the minimum and maximum y-axis limits.
+        """
+
+        if self.ignore_outliers:
+            dy = np.ptp(y) * 0.02
+            # Set Y scale to exclude outliers
+            q1, q3 = np.quantile(y, (0.25, 0.75))
+            iqr = q3 - q1  # Interquartile range
+            y_min = q1 - iqr * 1.5
+            y_max = q3 + iqr * 1.5
+
+            # Keep within range of data
+            y_min = max(y_min, min(y) - dy)
+            y_max = min(y_max, max(y) + dy)
+        else:
+            # Set Y scale to show all points
+            dy = np.ptp(y) * 0.02
+            y_min = min(y) - dy
+            y_max = max(y) + dy
+
+        # For log scale, low cannot be 0
+        if self.log_scale:
+            y_min = max(y_min, 1e-8)
+
+        return y_min, y_max
+
+    def resize_axes(self, x, y):
+        """Resize axes to fit data.
+
+        This is only called when plotting batches.
+
+        Args:
+            x: The x-coordinates of the data points.
+            y: The y-coordinates of the data points.
+        """
+
+        # Set X scale to show all points
+        x_min, x_max = self.calculate_xlim(x)
+        self.axes.set_xlim(x_min, x_max)
+
+        # Set Y scale
+        y_min, y_max = self.calculate_ylim(y)
+        self.axes.set_ylim(y_min, y_max)
+
+        # Add gridlines at midpoint between major ticks (major gridlines are automatic)
+        self._add_midpoint_gridlines()
+
+        # Redraw the plot
+        self.redraw_plot()
+
+    def redraw_plot(self):
+        """Redraw the plot."""
+
+        self.fig.canvas.draw_idle()
+
+    @staticmethod
+    def get_training_epoch_and_runtime_text(epoch: int, dt_min: int, dt_sec: int):
+        """Get the training epoch and runtime text to display in the plot.
+
+        Args:
+            epoch: The current epoch.
+            dt_min: The number of minutes since training started.
+            dt_sec: The number of seconds since training started.
+        """
+
+        runtime_text = (
+            r"Training Epoch $\mathbf{" + str(epoch + 1) + r"}$ / "
+            r"Runtime: $\mathbf{" + f"{int(dt_min):02}:{int(dt_sec):02}" + r"}$"
+        )
+
+        return runtime_text
+
+    @staticmethod
+    def get_eta_text(mean_epoch_time_min, mean_epoch_time_sec, eta_ten_epochs_min):
+        """Get the mean time and ETA text to display in the plot.
+
+        Args:
+            mean_epoch_time_min: The mean time per epoch in minutes.
+            mean_epoch_time_sec: The mean time per epoch in seconds.
+            eta_ten_epochs_min: The estimated time for the next ten epochs in minutes.
+        """
+
+        runtime_text = (
+            r"Mean Time per Epoch: $\mathbf{"
+            + f"{int(mean_epoch_time_min):02}:{int(mean_epoch_time_sec):02}"
+            + r"}$ / "
+            r"ETA Next 10 Epochs: $\mathbf{" + f"{int(eta_ten_epochs_min)}" + r"}$ min"
+        )
+
+        return runtime_text
+
+    @staticmethod
+    def get_epochs_in_plateau_text(epochs_in_plateau, plateau_patience):
+        """Get the epochs in plateau text to display in the plot.
+
+        Args:
+            epochs_in_plateau: The number of epochs in plateau.
+            plateau_patience: The number of epochs to wait before stopping training.
+        """
+
+        plateau_text = (
+            r"Epochs in Plateau: $\mathbf{" + f"{epochs_in_plateau}" + r"}$ / "
+            r"$\mathbf{" + f"{plateau_patience}" + r"}$"
+        )
+
+        return plateau_text
+
+    @staticmethod
+    def get_last_validation_loss_text(last_epoch_val_loss):
+        """Get the last epoch validation loss text to display in the plot.
+
+        Args:
+            last_epoch_val_loss: The validation loss from the last epoch.
+        """
+
+        last_val_loss_text = (
+            "Last Epoch Validation Loss: "
+            r"$\mathbf{" + f"{last_epoch_val_loss:.3e}" + r"}$"
+        )
+
+        return last_val_loss_text
+
+    @staticmethod
+    def get_best_validation_loss_text(best_val_y, best_epoch):
+        """Get the best epoch validation loss text to display in the plot.
+
+        Args:
+            best_val_x: The epoch number of the best validation loss.
+            best_val_y: The best validation loss.
+        """
+
+        best_val_loss_text = (
+            r"Best Epoch Validation Loss: $\mathbf{"
+            + f"{best_val_y:.3e}"
+            + r"}$ (epoch $\mathbf{"
+            + str(best_epoch)
+            + r"}$)"
+        )
+
+        return best_val_loss_text
+
+    @staticmethod
+    def add_with_newline(old_text: str, new_text: str):
+        """Add a new line to the text.
+
+        Args:
+            old_text: The existing text.
+            new_text: The text to add on a new line.
+        """
+
+        return old_text + "\n" + new_text
+
+    def update_runtime_title(
+        self,
+        epoch: int,
+        dt_min: int,
+        dt_sec: int,
+        last_epoch_val_loss: float = None,
+        penultimate_epoch_val_loss: float = None,
+        mean_epoch_time_min: int = None,
+        mean_epoch_time_sec: int = None,
+        eta_ten_epochs_min: int = None,
+        epochs_in_plateau: int = None,
+        plateau_patience: int = None,
+        epoch_in_plateau_flag: bool = False,
+        best_val_x: int = None,
+        best_val_y: float = None,
+        epoch_size: int = None,
+    ):
+
+        # Add training epoch and runtime info
+        title = self.get_training_epoch_and_runtime_text(epoch, dt_min, dt_sec)
+
+        if last_epoch_val_loss is not None:
+
+            if penultimate_epoch_val_loss is not None:
+                # Add mean epoch time and ETA for next 10 epochs
+                eta_text = self.get_eta_text(
+                    mean_epoch_time_min, mean_epoch_time_sec, eta_ten_epochs_min
+                )
+                title = self.add_with_newline(title, eta_text)
+
+                # Add epochs in plateau if flag is set
+                if epoch_in_plateau_flag:
+                    plateau_text = self.get_epochs_in_plateau_text(
+                        epochs_in_plateau, plateau_patience
+                    )
+                    title = self.add_with_newline(title, plateau_text)
+
+            # Add last epoch validation loss
+            last_val_text = self.get_last_validation_loss_text(last_epoch_val_loss)
+            title = self.add_with_newline(title, last_val_text)
+
+            # Add best epoch validation loss if available
+            if best_val_x is not None:
+                best_epoch = (best_val_x // epoch_size) + 1
+                best_val_text = self.get_best_validation_loss_text(
+                    best_val_y, best_epoch
+                )
+                title = self.add_with_newline(title, best_val_text)
+
+        self.set_title(title)
+
+    def set_title(self, title):
+        """Set the title of the plot.
+
+        Args:
+            title: The title text to display.
+        """
+
+        self.axes.set_title(title, fontweight="light", fontsize="small", x=0.55, y=1.03)
+
+
 class LossViewer(QtWidgets.QMainWindow):
     """Qt window for showing in-progress training metrics sent over ZMQ."""
 
