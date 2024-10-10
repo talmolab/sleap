09046813be2be1e368d0a0323337b21516f9a367
roomrys <38435167+roomrys@users.noreply.github.com>
2024-10-10 14:31:31 -0700

    Disable annoying albumentations update message

--- .conda/sleap_activate.sh
+++ .conda/sleap_activate.sh
@@ -7,3 +7,5 @@ export SLEAP_OLD_XLA_FLAGS=$XLA_FLAGS
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 # Help XLA find CUDA
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX
+# Disable annoying albumentations message
+export NO_ALBUMENTATIONS_UPDATE=1
