aa8771e21ea46619187302899e4ca3292d449ec3
Jvshen <justin2003.shen@gmail.com>
2024-10-25 01:05:45 -0700

    Replaced color_by_instance_group with distinctly_color to align with the existing View > Apply Distinct Colors To submenu

--- sleap/prefs.py
+++ sleap/prefs.py
@@ -15,7 +15,7 @@ class Preferences(object):
         "medium step size": 10,
         "large step size": 100,
         "color predicted": False,
-        "color_by_instance_group": False,
+        "distinctly_color": "instances",
         "propagate track labels": True,
         "palette": "standard",
         "bold lines": False,
