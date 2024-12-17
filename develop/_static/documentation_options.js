a654d134e377c8ec4c434f74327d73922dab08fc
talmo <talmo@users.noreply.github.com>
2024-12-17 18:11:46 +0000

    deploy: 5acb88f94f904c76fabfb38c9a857525fdb610c1

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.4.1a2',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'v1.4.1a3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -8,7 +7,7 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
