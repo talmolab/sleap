7e9354be5f0e2210f209ecf35cb141afa55f1eb6
roomrys <roomrys@users.noreply.github.com>
2024-12-19 17:52:41 +0000

    deploy: 7991f14dcf31877f0363f1f2a4392586a04d6eee

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.3.3',
-    LANGUAGE: 'None',
+    VERSION: 'v1.4.1',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
