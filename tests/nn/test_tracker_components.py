b425d39fc0e8328915dfcf051f80625ca28f9ec4
roomrys <38435167+roomrys@users.noreply.github.com>
2025-03-31 15:03:43 -0700

    Pass in the arguments we want to test with

--- tests/nn/test_tracker_components.py
+++ tests/nn/test_tracker_components.py
@@ -23,7 +23,7 @@ from sleap.skeleton import Skeleton
 @pytest.mark.parametrize("count", [0, 2])
 def test_tracker_by_name(tracker, similarity, match, count):
     t = Tracker.make_tracker_by_name(
-        "flow", "instance", "greedy", clean_instance_count=2
+        tracker=tracker, similarity=similarity, match=match, clean_instance_count=count
     )
     t.track([])
     t.final_pass([])
