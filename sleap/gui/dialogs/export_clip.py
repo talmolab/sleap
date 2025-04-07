5ca09541c5bc91aee1dac1e789f8f40f3458b532
roomrys <38435167+roomrys@users.noreply.github.com>
2025-04-07 16:10:00 -0700

    Reuse ExportClipDialog

--- sleap/gui/dialogs/export_clip.py
+++ sleap/gui/dialogs/export_clip.py
@@ -7,10 +7,11 @@ from qtpy import QtWidgets
 
 
 class ExportClipDialog(FormBuilderModalDialog):
-    def __init__(self):
+    def __init__(self, form_name=None):
         from sleap.io.videowriter import VideoWriter
 
-        super().__init__(form_name="labeled_clip_form")
+        form_name = form_name or "video_clip_form"
+        super().__init__(form_name=form_name)
 
         can_use_ffmpeg = VideoWriter.can_use_ffmpeg()
 
@@ -28,54 +29,3 @@ class ExportClipDialog(FormBuilderModalDialog):
         self.add_message(message)
 
         self.setWindowTitle("Export Clip Options")
-
-
-class ExportClipAndLabelsDialog(FormBuilderModalDialog):
-    def __init__(self, video_fps=30):
-        from sleap.io.videowriter import VideoWriter
-
-        # Initialize with a blank widget (no YAML needed)
-        super().__init__(form_widget=QtWidgets.QWidget())
-
-        self.setWindowTitle("Export Clip Options")
-
-        # FPS Field
-        self.fps_input = QtWidgets.QSpinBox()
-        self.fps_input.setRange(1, 240)
-        self.fps_input.setValue(video_fps)  # Set default FPS from video
-        self.add_message("Frames per second:")
-        self.layout().insertWidget(2, self.fps_input)
-
-        # Open when done Checkbox
-        self.open_when_done = QtWidgets.QCheckBox("Open file after saving")
-        self.layout().insertWidget(3, self.open_when_done)
-
-        # Video format message
-        can_use_ffmpeg = VideoWriter.can_use_ffmpeg()
-        if can_use_ffmpeg:
-            message = (
-                "<i><b>MP4</b> file will be encoded using "
-                "system ffmpeg via imageio (preferred option).</i>"
-            )
-        else:
-            message = (
-                "<i>Unable to use ffmpeg via imageio. "
-                "<b>AVI</b> file will be encoded using OpenCV.</i>"
-            )
-        self.add_message(message)
-
-    def _get_form_results(self):
-        """Get form results as a dictionary."""
-        return {
-            "fps": self.fps_input.value(),
-            "open_when_done": self.open_when_done.isChecked(),
-        }
-
-    def on_accept(self):
-        """Retrieve the form results and accept the dialog."""
-        self._results = self._get_form_results()
-        self.accept()
-
-    def get_results(self):
-        """Get results as a dictionary."""
-        return self._get_form_results()
