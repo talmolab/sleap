44450e3e8b791a6f331987133c90472f0e73c03f
AmickL <amickl29@yahoo.com>
2025-09-09 11:02:52 -0700

    change: allow duplicated instance nodes to be moveable & ungrabbable

--- sleap/gui/widgets/video.py
+++ sleap/gui/widgets/video.py
@@ -2166,7 +2166,10 @@ class QtInstance(QGraphicsObject):
     def duplicate_instance(self):
         """Duplicate the instance and add it to the scene."""
         # Add instance to the context
+        print('duplicate instance method called')
+
         if self.player.context is None:
+            print('self.player.context is None, cannot duplicate instance')
             return
 
         # Copy the instance and add it to the context
@@ -2195,11 +2198,15 @@ class QtInstance(QGraphicsObject):
                     # Set this QtInstance to be movable
                     qt_inst.setFlag(QGraphicsItem.ItemIsMovable)
 
+                    # Set all nodes to be movable
+                    for node in qt_inst.nodes.values():
+                        node.setFlag(QGraphicsItem.ItemIsMovable, True)
+
                     # Optionally grab the mouse and change cursor, so user can
                     # immediately drag
                     qt_inst.setCursor(Qt.ClosedHandCursor)
                     qt_inst.grabMouse()
-                    break
+            
 
         # Connect the callback to the updatedSelection signal
         self.player.view.updatedSelection.connect(on_selection_update)
@@ -2217,11 +2224,12 @@ class QtInstance(QGraphicsObject):
 
     def mouseReleaseEvent(self, event):
         """Custom event handler for mouse release."""
+        # self.ungrabMouse() causes QGraphicsItem::ungrabMouse: not a mouse grabber warning
         if self.flags() & QGraphicsItem.ItemIsMovable:
             self.setFlag(QGraphicsItem.ItemIsMovable, False)
             self.updatePoints(user_change=True)
             self.updateBox()
-            # self.ungrabMouse()
+            self.ungrabMouse()
             super().mouseReleaseEvent(event)
 
 
